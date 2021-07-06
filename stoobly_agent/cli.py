e177cc270df545c4719d0b96b407418f05df8bd8
jvlythical <lvlichael8@gmail.com>
2021-07-06 16:58:19 -0700

    Update run command to start both proxy and ui

--- stoobly_agent/cli.py
+++ stoobly_agent/cli.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import click
+import pdb
+import threading
 
 from stoobly_agent.proxy import run as run_proxy
 from stoobly_agent.api import run as run_api
@@ -16,12 +18,26 @@ def main(ctx):
     pass
 
 @main.command()
-@click.option('--listen-host', default='0.0.0.0', help='Address to bind proxy to.')
-@click.option('--listen-port', default=8080, help='Proxy service port.')
-@click.option('--headless', help='Disable starting frontend.')
+@click.option('--proxy-host', default='0.0.0.0', help='Address to bind proxy to.')
+@click.option('--proxy-port', default=8080, help='Proxy service port.')
+@click.option('--ui-host', default='0.0.0.0', help='Address to bind UI to.')
+@click.option('--ui-port', default=4200, help='UI service port.')
+@click.option('--headless', default=False, help='Disable starting frontend.')
 def run(**kwargs):
-    #run_proxy(**kwargs)
+    if not kwargs['headless']:
+        thread = threading.Thread(target=run_api, args=(kwargs.get('ui_host'), kwargs.get('ui_port')))
+        thread.start()
 
-    run_api('0.0.0.0', 3001)
+    # Filter out non-mitmproxy options
+    kwargs['listen_host'] = kwargs['proxy_host']
+    kwargs['listen_port'] = kwargs['proxy_port']
+
+    del kwargs['proxy_host']
+    del kwargs['proxy_port']
+    del kwargs['ui_host']
+    del kwargs['ui_port']
+    del kwargs['headless']
+
+    run_proxy(**kwargs)
 
 
