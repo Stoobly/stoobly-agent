5c720b1d974cdd34d9d6625e6a13348b359f5886
jvlythical <lvlichael8@gmail.com>
2021-09-01 22:04:32 -0700

    Create new dump-config command, add --api-url flag for run (#6)
    * Create new dump-config command
    
    - Useful for dumping all the config and settings for debugging
    - Also add `--api-url` flag for the `run` command
    
    * bug fixes
    
    * Extract into Settings methods
    
    * Also dump env vars
    
    * pr review changes
    
    * no save to file method
    
    * bump version
    
    * minor tweaks from PR review
    
    * bump release version
    
    Co-authored-by: stoobmmelier <>

--- stoobly_agent/cli.py
+++ stoobly_agent/cli.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 import click
+import json
 import os
 import pdb
+import time
 import threading
 
 from .api import run as run_api
@@ -36,6 +38,7 @@ def main(ctx):
 @click.option('--proxy-port', default=8080, help='Proxy service port.')
 @click.option('--ui-host', default='0.0.0.0', help='Address to bind UI to.')
 @click.option('--ui-port', default=4200, help='UI service port.')
+@click.option('--api-url', help='API URL.')
 def run(**kwargs):
     if not os.getenv(LOG_LEVEL):
         os.environ[LOG_LEVEL] = kwargs['log_level']
@@ -44,10 +47,32 @@ def run(**kwargs):
     settings.proxy_url = f"http://{kwargs['proxy_host']}:{kwargs['proxy_port']}"
     settings.agent_url = f"http://{kwargs['ui_host']}:{kwargs['ui_port']}"
 
+    if kwargs['api_url']:
+        settings.api_url = kwargs['api_url']
+
     if not kwargs['headless']:
         initialize_ui(kwargs)
 
     initialize_proxy(kwargs)
+
+@main.command()
+@click.option('--pretty-print', is_flag=True, default=False, help='Pretty print the json.')
+@click.option('--save-to-file', is_flag=True, default=False, help='To save to a file or not.')
+def dump_config(**kwargs):
+    settings = Settings.instance()
+    
+    output = settings.to_json(pretty_print=kwargs['pretty_print'])
+
+    if kwargs['save_to_file']:
+        timestamp = str(int(time.time() * 1000))
+        config_dump_file_name = f"stoobly_agent_config_dump_{timestamp}.json"
+
+        with open(config_dump_file_name, 'w') as output_file:
+            output_file.write(output)
+
+        print(f"Config successfully dumped to {config_dump_file_name}")
+    else:
+        print(output)
 
 ### Helpers
 
@@ -73,5 +98,6 @@ def initialize_proxy(kwargs):
     del options['proxy_port']
     del options['ui_host']
     del options['ui_port']
+    del options['api_url']
 
     run_proxy(**options)
