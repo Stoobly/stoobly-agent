f8cb7792144b76ea4f6b31aac65a1e222fd22be2
stoobmmelier <87163625+stoobmmelier@users.noreply.github.com>
2021-10-07 21:15:24 -0700

    Add CI jobs (#9)
    - Run flake8 linter for every commit to PR and push to master
    - Publish
      - Download dashboard from Artifactory
      - Docker build
      - Docker push
      - twine upload to PyPI
    
    - Secrets added in https://github.com/Stoobly/stoobly-agent/settings/secrets/actions
    
    Co-authored-by: stoobmmelier <>

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM python:3.8.11-slim
 
-RUN pip install stoobly-agent
+RUN mkdir -p /app
+WORKDIR /app
+COPY . /app
+RUN pip install .
 
 EXPOSE 8080
 EXPOSE 4200
--- setup.py
+++ setup.py
@@ -30,5 +30,5 @@ setup(
     },
     #scripts=['bin/stoobly-agent'],
     url='https://github.com/Stoobly/stoobly-agent',
-    version='0.1.9',
+    version='0.1.10',
 )
