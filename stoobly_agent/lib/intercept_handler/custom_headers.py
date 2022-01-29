95f0aa8ef10dc749c1803a727575970b6a84d544
jvlythical <lvlichael8@gmail.com>
2022-01-28 21:51:14 -0800

    Add MOCK_REQUEST_ID to custom_headers

--- stoobly_agent/lib/intercept_handler/custom_headers.py
+++ stoobly_agent/lib/intercept_handler/custom_headers.py
@@ -1,5 +1,6 @@
 CUSTOM_HEADERS = {
     'MOCK_POLICY': 'X-Mock-Policy',
+    'MOCK_REQUEST_ID': 'X-Mock-Request-Id',
     'DO_PROXY': 'X-Do-Proxy',
     'PROXY_MODE': 'X-Proxy-Mode',
     'RECORD_POLICY': 'X-Record-Policy',
