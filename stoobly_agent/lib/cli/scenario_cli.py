25debb3e72ece3b1ee878b955d9e0a1702a96059
Michael Yen <Michael Yen>
2022-03-03 01:23:03 -0800

    Add scenarios list command

--- stoobly_agent/lib/cli/scenario_cli.py
+++ stoobly_agent/lib/cli/scenario_cli.py
@@ -1,4 +1,6 @@
 import click
+import json
+import pdb
 
 from ..settings import Settings
 from .scenario import Scenario
@@ -13,8 +15,23 @@ def scenario(ctx):
 @click.option('--strategy', help='Test strategy')
 @click.argument('scenario_key')
 def test(**kwargs):
-    scenario = Scenario(Settings.instance(), kwargs['scenario_key'])
-    scenario.test(
-        report_key=kwargs['save_to_report'],
-        strategy=kwargs['strategy']
-    )
+    scenario_key = kwargs['scenario_key']
+    del kwargs['scenario_key']
+
+    scenario = Scenario(Settings.instance())
+    scenario.test(scenario_key, **kwargs)
+
+@scenario.command()
+@click.option('--page', default=0)
+@click.option('--sort-by', default='created_at', help='created_at|name')
+@click.option('--sort-order', default='desc', help='asc | desc')
+@click.option('--size', default=10)
+@click.argument('project_key')
+def list(**kwargs):
+    project_key = kwargs['project_key']
+    del kwargs['project_key']
+
+    scenario = Scenario(Settings.instance())
+    scenarios = scenario.index(project_key, **kwargs)
+
+    print(json.dumps(scenarios, indent=2))
