3a71c97b4f9f7e9e9f9ba18ee8bdc290c7776200
Michael Yen <Michael Yen>
2022-03-17 17:56:26 -0700

    Refactor handle_record_service.py

--- stoobly_agent/app/proxy/handle_record_service.py
+++ stoobly_agent/app/proxy/handle_record_service.py
@@ -4,9 +4,10 @@ from mitmproxy.http import HTTPFlow as MitmproxyHTTPFlow
 from mitmproxy.net.http.request import Request as MitmproxyRequest
 from mitmproxy.net.http.response import Response as MitmproxyResponse 
 
-from stoobly_agent.lib.api.requests_resource import RequestsResource
+from stoobly_agent.lib.models.request_model import RequestModel
 from stoobly_agent.lib.logger import Logger
 from stoobly_agent.lib.settings import IProjectRecordSettings, Settings
+from stoobly_agent.lib.settings.active_mode_settings_builder import ActiveModeSettingsBuilder
 
 from .constants import custom_response_codes, record_policy
 from .mock.eval_request_service import inject_eval_request
@@ -36,28 +37,20 @@ def handle_response_record(flow: MitmproxyHTTPFlow, settings: Settings):
     __disable_transfer_encoding(flow.response)
 
     request: MitmproxyRequest = flow.request
+    active_mode_settings: IProjectRecordSettings = __get_active_mode_settings(settings)
 
-    active_mode_settings: IProjectRecordSettings = settings.active_mode_settings
-
-    if active_mode_settings.get('enabled') and allowed_request(active_mode_settings, request):
-        upload_policy = get_record_policy(request.headers, active_mode_settings)
-    else:
-        # If the request path does not match accepted paths, do not record
-        upload_policy = record_policy.NONE
+    upload_policy = __get_upload_policy(request, active_mode_settings)
 
     Logger.instance().debug(f"{LOG_ID}:UploadPolicy: {upload_policy}")
 
     if upload_policy == record_policy.ALL:
-        api = RequestsResource(settings.api_url, settings.api_key)
-        thread = threading.Thread(target=inject_upload_request(api, settings), args=[flow])
-        thread.start()
+        __upload_request(flow, settings)
     elif upload_policy == record_policy.NOT_FOUND:
-        api = RequestsResource(settings.api_url, settings.api_key)
-        res = inject_eval_request(api, active_mode_settings)(request, [])
+        request_model = RequestModel(settings)
+        res = inject_eval_request(request_model, active_mode_settings)(request, [])
 
         if res.status_code == custom_response_codes.NOT_FOUND:
-            thread = threading.Thread(target=inject_upload_request(api, settings), args=[flow])
-            thread.start()
+            __upload_request(flow, settings, request_model)
     elif upload_policy == record_policy.NONE:
         pass
     else:
@@ -66,6 +59,26 @@ def handle_response_record(flow: MitmproxyHTTPFlow, settings: Settings):
             "Valid env RECORD_POLICY: %s, %s, %s, Got: %s" %
             [record_policy.ALL, record_policy.NOT_FOUND, record_policy.NONE, upload_policy]
         )
+
+def __upload_request(flow: MitmproxyHTTPFlow, settings: Settings, request_model=None):
+    if not request_model:
+        request_model = RequestModel(settings)
+
+    thread = threading.Thread(target=inject_upload_request(request_model, settings), args=[flow])
+    thread.start()
+
+def __get_active_mode_settings(settings: Settings):
+    if settings.remote_enabled:
+        return settings.active_mode_settings
+    else:
+        return ActiveModeSettingsBuilder(settings.active_mode).build()
+
+def __get_upload_policy(request: MitmproxyRequest, active_mode_settings: IProjectRecordSettings):
+    if active_mode_settings.get('enabled') and allowed_request(active_mode_settings, request):
+        return get_record_policy(request.headers, active_mode_settings)
+    else:
+        # If the request path does not match accepted paths, do not record
+        return record_policy.NONE
 
 def __disable_transfer_encoding(response: MitmproxyResponse):
     header_name = 'Transfer-Encoding'
