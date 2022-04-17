e224a1ab80a86bf22e824ff21f22b4021d318e13
Michael Yen <Michael Yen>
2022-04-16 17:52:20 -0700

    Create stoobly-agent scenario show command

--- stoobly_agent/app/cli/scenario_facade.py
+++ stoobly_agent/app/cli/scenario_facade.py
@@ -2,23 +2,24 @@ import pdb
 import requests
 from stoobly_agent.config.constants import test_strategy
 
-from stoobly_agent.lib.api.scenarios_resource import ScenariosResource
 from stoobly_agent.app.models.request_model import RequestModel
-from stoobly_agent.config.constants import mode
 from stoobly_agent.app.proxy.replay.replay_scenario_service import replay
 from stoobly_agent.app.settings import Settings
+from stoobly_agent.config.constants import mode
+from stoobly_agent.lib.api.interfaces.scenarios import ScenarioShowResponse, ScenariosIndexResponse
+from stoobly_agent.lib.api.keys import ProjectKey, ScenarioKey
+from stoobly_agent.lib.api.scenarios_resource import ScenariosResource
 
 class ScenarioFacade():
 
   def __init__(self, settings: Settings):
     self.settings = settings
+    self.__api = ScenariosResource(self.settings.remote.api_url, self.settings.remote.api_key)
 
   def create(self, project_key: str, name: str, description: str = ''):
-    api = ScenariosResource(self.settings.remote.api_url, self.settings.remote.api_key)
-
-    res: requests.Response = api.from_project_key(
+    res: requests.Response = self.__api.from_project_key(
       project_key, 
-      lambda project_id: api.create(
+      lambda project_id: self.__api.create(
         project_id, {
           'description': description,
           'name': name,
@@ -31,11 +32,14 @@ class ScenarioFacade():
 
     return res.json()
 
-  def index(self, project_key, **kwargs):
-    api = ScenariosResource(self.settings.remote.api_url, self.settings.remote.api_key)
-    res = api.from_project_key(
-      project_key, lambda project_id: api.index(project_id, kwargs)
-    ) 
+  def index(self, project_key, **kwargs) -> ScenariosIndexResponse:
+    key = ProjectKey(project_key)
+    res = self.__api.index(**{ 'project_id': key.id, **kwargs})
+    return res.json()
+
+  def show(self, scenario_key: str) -> ScenarioShowResponse:
+    key = ScenarioKey(scenario_key)
+    res = self.__api.show(key.id)
     return res.json()
 
   def replay(self, scenario_key: str, **kwargs):
--- stoobly_agent/lib/api/scenarios_resource.py
+++ stoobly_agent/lib/api/scenarios_resource.py
@@ -1,8 +1,8 @@
+import requests
 import urllib
 
 from ..logger import Logger
 from .interfaces.pagination_query_params import PaginationQueryParams
-from .interfaces.scenarios_index_response import ScenariosIndexResponse
 from .stoobly_api import StooblyApi
 
 class ScenariosResource(StooblyApi):
@@ -22,15 +22,16 @@ class ScenariosResource(StooblyApi):
 
     return self.post(url, headers=self.default_headers, data=body, files=files)
 
-  def index(self, project_id: int, query_params: PaginationQueryParams) -> ScenariosIndexResponse:
+  def index(self, **query_params: PaginationQueryParams) -> requests.Response:
     url = f"{self.service_url}/{self.SCENARIOS_ENDPOINT}"
 
-    params = {
-      'project_id': project_id,
-      **query_params,
-    }
+    Logger.instance().debug(f"{self.LOG_ID}.request_response:{url}?{urllib.parse.urlencode(query_params)}")
 
-    Logger.instance().debug(f"{self.LOG_ID}.request_response:{url}?{urllib.parse.urlencode(params)}")
+    return self.get(url, headers=self.default_headers, params=query_params)
 
-    return self.get(url, headers=self.default_headers, params=params)
+  def show(self, scenario_id: int, **query_params) -> requests.Response:
+    url = f"{self.service_url}/{self.SCENARIOS_ENDPOINT}/{scenario_id}"
 
+    Logger.instance().debug(f"{self.LOG_ID}.request_response:{url}?{urllib.parse.urlencode(query_params)}")
+
+    return self.get(url, headers=self.default_headers, params=query_params)
