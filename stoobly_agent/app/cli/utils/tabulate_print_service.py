90da57f2b1d48e0f91fef97ba27b627e34c161b0
jvlythical <lvlichael8@gmail.com>
2022-04-16 17:49:57 -0700

    Update tabulate_print_service.py to support headers and select

--- stoobly_agent/app/cli/utils/tabulate_print_service.py
+++ stoobly_agent/app/cli/utils/tabulate_print_service.py
@@ -4,21 +4,30 @@ from tabulate import tabulate
 from typing import List, TypedDict
 
 class TabulatePrintOptions(TypedDict):
-  filter: dict
+  filter: list
   format: str
+  headers: bool
+  select: list
 
 def tabulate_print(records: List[dict], **kwargs: TabulatePrintOptions):
-  filter = kwargs.get('filter') or {}
-  format = kwargs.get('format') or 'orgtbl'
+  show_header = kwargs.get('headers')
+  filter = kwargs.get('filter') or []
+  format = kwargs.get('format') or 'plain'
+  select = kwargs.get('select') or []
 
   headers = []
   rows = []
   for record in records:
-    build_header = len(headers) == 0
+    build_header = show_header and len(headers) == 0
 
     row = []
     for key, val in record.items():
+      # If exclude list is provided, check if key is excluded
       if key in filter:
+        continue
+      
+      # If include list is provided, check if key is included
+      if len(select) > 0 and key not in select:
         continue
 
       if build_header:
