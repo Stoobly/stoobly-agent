310d809b61aed5369aff3c9455c52e8794d1564c
Michael Yen <Michael Yen>
2022-04-26 21:43:41 -0700

    Refactor

--- stoobly_agent/app/settings/__init__.py
+++ stoobly_agent/app/settings/__init__.py
@@ -1,6 +1,4 @@
-import json
 import os
-import time
 import yaml
 import pdb
 
@@ -14,12 +12,10 @@ from stoobly_agent.config.data_dir import DataDir
 from stoobly_agent.config.source_dir import SourceDir
 from stoobly_agent.lib.logger import Logger
 
-#from .helpers.active_mode_settings_builder import ActiveModeSettingsBuilder
 from .cli_settings import CLISettings
 from .proxy_settings import ProxySettings
 from .remote_settings import RemoteSettings
 from .ui_settings import UISettings
-from .types import IProjectModeSettings
 
 class Settings:
     LOG_ID = 'app.settings'
@@ -80,21 +76,6 @@ class Settings:
     def remote(self):
         return self.__remote_settings
 
-    # Get active mode settings first from environment variables
-    # If the env var is null, get the setting from the yaml file
-    @property
-    def active_mode_settings(self) -> IProjectModeSettings:
-        if self.cli.remote_enabled:
-            active_mode_settings = self.__build_active_mode_settings()
-
-            if self.ui.active:
-                self.__override_settings_with_env(active_mode_settings)
-                self.__override_project_settings_with_env(active_mode_settings)
-
-            return active_mode_settings
-        else:
-            return ActiveModeSettingsBuilder(self.proxy.intercept.active).with_enabled(True).build()
-
     def watch(self):
         patterns = ['settings.yml']
         ignore_patterns = None
@@ -130,84 +111,6 @@ class Settings:
         self.write(self.to_dict())
 
     ### Helpers
-
-    #
-    # Return active project's settings
-    # If a scenario is set, return scenario settings
-    #
-    def __build_active_mode_settings(self) -> IProjectModeSettings:
-        mode = self.mode
-
-        if not mode:
-            return {}
-
-        active_mode = self.proxy.intercept.active
-
-        if not active_mode:
-            return {}
-
-        active_mode_settings = mode.get(active_mode, {})
-        all_project_settings = active_mode_settings.get('settings', {})
-
-        project_key = active_mode_settings.get('project_key')
-        project_settings = all_project_settings.get(project_key)
-
-        if not project_settings:
-            project_settings = {}
-        else:
-            scenario_key = project_settings.get('scenario_key')
-            if scenario_key and len(scenario_key) != 0:
-                project_settings = all_project_settings.get(scenario_key) or {}
-
-                project_settings['scenario_key'] = scenario_key
-            else:
-                project_settings['scenario_key'] = ''
-
-        # Merge higher level settings
-        project_settings['project_key'] = project_key
-        project_settings['enabled'] = active_mode_settings.get('enabled')
-
-        return project_settings
-
-    def __override_project_settings_with_env(self, active_mode_settings: IProjectModeSettings):
-        include_patterns = os.environ.get(env_vars.AGENT_INCLUDE_PATTERNS)
-        if include_patterns != None:
-            # Split the string based on commas, strip whitespace
-            active_mode_settings['include_patterns'] = list(map(str.strip, include_patterns.split(',')))
-
-        exclude_patterns = os.environ.get(env_vars.AGENT_EXCLUDE_PATTERNS)
-        if exclude_patterns != None:
-            # Split the string based on commas, strip whitespace
-            active_mode_settings['exclude_patterns'] = list(map(str.strip, exclude_patterns.split(',')))
-
-        policy = os.environ.get(env_vars.AGENT_POLICY)
-        if policy != None:
-            active_mode_settings['policy'] = policy
-
-        proxy_scenario_key = os.environ.get(env_vars.AGENT_SCENARIO_KEY)
-        if proxy_scenario_key != None:
-            active_mode_settings['scenario_key'] = proxy_scenario_key
-
-        proxy_service_url = os.environ.get(env_vars.AGENT_SERVICE_URL)
-        if proxy_service_url != None:
-            active_mode_settings['service_url'] = proxy_service_url
-
-    ###
-    #
-    # Replace setting with env var if set
-    #
-    # @param active_mode_settings [Dict]
-    #
-    def __override_settings_with_env(self, active_mode_settings: IProjectModeSettings):
-        enabled = os.environ.get(env_vars.AGENT_ENABLED)
-
-        if enabled != None:
-            active_mode_settings['enabled'] = not not enabled
-
-        proxy_project_key = os.environ.get(env_vars.AGENT_PROJECT_KEY)
-        if proxy_project_key != None:
-            active_mode_settings['project_key'] = proxy_project_key
-
 
     def __load_settings(self):
         with open(self.__settings_file_path, 'r') as stream:
