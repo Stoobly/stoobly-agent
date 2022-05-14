05db17e956ef6ecdba88aa68f8d0ba4bea665b5f
Michael Yen <Michael Yen>
2022-05-14 12:05:45 -0700

    Update endpoints interface to support EndpointShowResposne

--- stoobly_agent/lib/api/interfaces/endpoints.py
+++ stoobly_agent/lib/api/interfaces/endpoints.py
@@ -4,3 +4,23 @@ from .pagination_query_params import PaginationQueryParams
 
 class EndpointsIndexQueryParams(PaginationQueryParams):
   project_id: str
+
+class RequestComponentName(TypedDict):
+  alias_id: int
+  name: str
+  value: str
+
+class Alias(TypedDict):
+  id: int
+  name: str
+
+class ResponseParamName(RequestComponentName):
+  query: str
+
+class EndpointShowResponse(TypedDict):
+  aliases: List[Alias]
+  body_param_names: List[RequestComponentName]
+  header_names: List[RequestComponentName]
+  path_segment_names: List[RequestComponentName]
+  query_param_names: List[RequestComponentName]
+  response_param_names: List[ResponseParamName]
