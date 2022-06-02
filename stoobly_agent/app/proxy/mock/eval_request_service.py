5f80e1d43e2fb59be13ba07c7ef0fe457d3e597d
Michael Yen <Michael Yen>
2022-06-02 15:23:41 -0700

    Add support for X-Stoobly-Request-Id when mocking a request

--- stoobly_agent/app/proxy/mock/eval_request_service.py
+++ stoobly_agent/app/proxy/mock/eval_request_service.py
@@ -4,6 +4,7 @@ import pdb
 from mitmproxy.net.http.request import Request as MitmproxyRequest
 from requests import Response
 from typing import List, TypedDict, Union
+from stoobly_agent.config.constants import custom_headers
 
 from stoobly_agent.lib.api.param_builder import ParamBuilder
 from stoobly_agent.app.models.request_model import RequestModel
@@ -50,20 +51,9 @@ def eval_request(
     ignored_components = __build_ignored_components(ignored_components_list or [])
     query_params.with_params(__build_request_params(request, ignored_components))
 
-    query_params.with_params(__build_optional_params(options))
+    query_params.with_params(__build_optional_params(request, options))
 
     return request_model.response(**query_params.build())
-
-def __build_optional_params(options: EvalRequestOptions):
-    optional_params = {}
-
-    if options.get('retry'):
-        optional_params['retry'] = options['retry']
-
-        if options.get('infer'):
-            optional_params['infer'] = options['infer']
-
-    return optional_params
 
 def __build_ignored_components(ignored_components_list):
     ignored_components = []
@@ -122,3 +112,18 @@ def __build_request_params(request: MitmproxyRequest, ignored_components = []) -
         query_params['body_text_hash'] = body_text_hash
 
     return query_params
+
+def __build_optional_params(request: MitmproxyRequest, options: EvalRequestOptions):
+    optional_params = {}
+
+    if options.get('retry'):
+        optional_params['retry'] = options['retry']
+
+        if options.get('infer'):
+            optional_params['infer'] = options['infer']
+
+    headers = request.headers
+    if custom_headers.MOCK_REQUEST_ID in headers:
+        optional_params['request_id'] = headers[custom_headers.MOCK_REQUEST_ID]
+
+    return optional_params
