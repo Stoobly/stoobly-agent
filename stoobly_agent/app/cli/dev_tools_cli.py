4c86f305a4b4e955aeaa81de250b708779544010
Michael Yen <Michael Yen>
2022-06-29 17:57:09 -0700

    Update dev-tools to import orm's

--- stoobly_agent/app/cli/dev_tools_cli.py
+++ stoobly_agent/app/cli/dev_tools_cli.py
@@ -2,8 +2,11 @@ import click
 import pdb
 
 from stoobly_agent.lib.orm.migrate_service import migrate as database_migrate
+from stoobly_agent.lib.orm.request import Request
+from stoobly_agent.lib.orm.response import Response
 from stoobly_agent.lib.orm.trace import Trace
 from stoobly_agent.lib.orm.trace_alias import TraceAlias
+from stoobly_agent.lib.orm.trace_request import TraceRequest
 
 @click.group(
   epilog="Run 'stoobly-agent dev-tools COMMAND --help' for more information on a command.",
