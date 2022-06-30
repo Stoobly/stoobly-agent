b608451a122cc8a7069bc7fe1fd5c85cd68a6083
jvlythical <lvlichael8@gmail.com>
2022-06-29 17:58:09 -0700

    Update ReplayCliOptions to accept trace_id as option

--- stoobly_agent/app/cli/helpers/trace_context_facade.py
+++ stoobly_agent/app/cli/helpers/trace_context_facade.py
@@ -5,13 +5,14 @@ from stoobly_agent.app.proxy.replay.trace_context import TraceContext
 
 from stoobly_agent.app.settings import Settings
 from stoobly_agent.lib.api.endpoints_resource import EndpointsResource
+from stoobly_agent.lib.orm.trace import Trace
 
 class TraceContextFacade():
 
-  def __init__(self, settings: Settings):
+  def __init__(self, settings: Settings, trace: Trace = None):
     self.__settings = settings
     self.__endpoints_resource = EndpointsResource(self.__settings.remote.api_url, self.__settings.remote.api_key)
-    self.__trace_context = TraceContext(self.__endpoints_resource)
+    self.__trace_context = TraceContext(self.__endpoints_resource, trace)
 
   @property
   def trace_context(self):
