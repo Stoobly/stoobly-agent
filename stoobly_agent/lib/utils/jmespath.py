717e5c6fd6d3949a468bda75ede2f649e548d487
jvlythical <lvlichael8@gmail.com>
2022-07-24 16:23:12 -0700

    Refactor jmespath Options to jmespath.py

--- stoobly_agent/lib/utils/jmespath.py
+++ stoobly_agent/lib/utils/jmespath.py
@@ -1,10 +1,16 @@
 import jmespath
 
+from typing import Callable, TypedDict, Union
+
 from .visitor import TreeInterpreter, Visitor
 
 # Monkey patch jmespath with replacement functionality
 jmespath.parser.visitor.Vistor = Visitor
 jmespath.parser.visitor.TreeInterpreter = TreeInterpreter
 
-def search(query, o, options = {}):
-  jmespath.search(query, o, options)
+class Options(TypedDict):
+  handle_after_replace: Callable
+  handle_replace: Callable
+
+def search(query: str, o: Union[dict, list], options: Options = {}):
+  return jmespath.search(query, o, options)
