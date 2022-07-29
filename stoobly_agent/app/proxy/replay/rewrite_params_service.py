e8bc2e11cc0f51dbe348b3e36f4e1c51216c7a91
jvlythical <lvlichael8@gmail.com>
2022-07-29 16:31:44 -0700

    Fix resolution of multiple extracted aliases from response

--- stoobly_agent/app/proxy/replay/rewrite_params_service.py
+++ stoobly_agent/app/proxy/replay/rewrite_params_service.py
@@ -2,6 +2,7 @@ import pdb
 
 from typing import Callable, Dict, List, TypedDict, Union
 
+from stoobly_agent.app.proxy.replay.alias_context import AliasContext
 from stoobly_agent.app.proxy.replay.alias_resolver import AliasResolver
 from stoobly_agent.config.constants import alias_resolve_strategy
 from stoobly_agent.lib.api.interfaces.endpoints import Alias, RequestComponentName
@@ -10,19 +11,12 @@ from stoobly_agent.lib.utils import jmespath
 
 AliasMap = Dict[str, RequestComponentName]
 
-def __handle_after_replace(trace: TraceAlias, value):
+def __handle_after_replace(name, value, trace: TraceAlias):
   pass
 
-def __handle_replace(replacement_options: list, number_of_replacements: int, strategy: alias_resolve_strategy.AliasResolveStrategy) -> int:
-  if strategy == alias_resolve_strategy.NONE:
-    return -1
-
-  if strategy == alias_resolve_strategy.FIFO:
-    index = number_of_replacements
-    return -1 if index >= len(replacement_options) else index
-  elif strategy == alias_resolve_strategy.LIFO:
-    index = len(replacement_options) - number_of_replacements - 1
-    return -1 if index < 0 else index
+def __handle_replace(name, value, alias_resolver: AliasContext):
+  trace_alias = alias_resolver.resolve_alias(name, value)
+  return trace_alias.value
 
 def build_id_to_alias_map(aliases: List[Alias]):
     id_to_alias = {}
@@ -41,20 +35,13 @@ def rewrite_params(
   if alias_resolver.strategy == alias_resolve_strategy.NONE:
     return
 
+  alias_context = AliasContext(alias_resolver)
+
   for param_name in param_names:
     _alias: Alias = id_to_alias.get(param_name['alias_id'])
+
     if not _alias:
       continue
-
-    current_value = jmespath.search(param_name.get('query') or param_name.get('name'), params)
-    trace_aliases = alias_resolver.resolve_aliases(_alias['name'], current_value)
-
-    if trace_aliases.is_empty():
-      continue
-
-    trace_aliases_list = []
-    trace_aliases.each(lambda trace_alias: trace_aliases_list.append(trace_alias))
-    trace_alias_values = list(map(lambda trace_alias: trace_alias.value, trace_aliases_list))
  
     handle_after_replace = options.get('handle_after_replace')
     handle_after_replace = handle_after_replace if handle_after_replace else __handle_after_replace
@@ -63,11 +50,10 @@ def rewrite_params(
     # If there's more than one, then try to assign different alias values 
     handle_replace = options.get('handle_replace')
     handle_replace = handle_replace if handle_replace else __handle_replace
-    
+
     jmespath.search(
       param_name['query'], params, { 
-        'replacements': trace_alias_values, 
-        'handle_replace': lambda options, count: __handle_replace(options, count, alias_resolver.strategy),
-        'handle_after_replace': lambda v, i: __handle_after_replace(trace_aliases_list[i], v)
+        'handle_replace': lambda name, value, i: __handle_replace(_alias['name'], value, alias_context),
+        'handle_after_replace': lambda name, value, i: __handle_after_replace(name, value, alias_context.resolved_aliases[i])
       }
     )
--- stoobly_agent/lib/utils/jmespath.py
+++ stoobly_agent/lib/utils/jmespath.py
@@ -17,3 +17,25 @@ def search(query: str, o: Union[dict, list], options: Options = {}):
 
 def compile(expression: str):
   return jmespath.compile(expression)
+
+def flatten(value, query: str) -> list:
+  array_count = query.count('[*]')
+
+  if array_count == 0 or not isinstance(value, list):
+    return [value]
+  else:
+    return __flatten(value, array_count)
+
+def __flatten(ar, depth, cur_depth = 0):
+  if cur_depth == depth:
+    return ar
+
+  next_ar = []
+  for el in ar:
+    if cur_depth == depth - 1:
+      next_ar.append(el)
+    else:
+      if isinstance(el, list):
+        next_ar.append(el)
+  
+  return __flatten(next_ar, depth, cur_depth + 1)
