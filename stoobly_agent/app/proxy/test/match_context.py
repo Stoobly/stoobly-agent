1174b08de402350d0da77fbda516c38e76a50af6
jvlythical <lvlichael8@gmail.com>
2022-08-28 02:42:52 -0700

    Add support for test match hooks

--- stoobly_agent/app/proxy/test/match_context.py
+++ stoobly_agent/app/proxy/test/match_context.py
@@ -2,6 +2,7 @@ from stoobly_agent.app.proxy.test.response_param_names_facade import ResponsePar
 from stoobly_agent.lib.api.interfaces.endpoints import ResponseParamName
 
 class IMatchContext(TypedDict):
+    lifecycle_hooks: dict
     path_key: str
     query: str
     response_param_names_facade: ResponseParamNamesFacade
@@ -11,10 +12,13 @@ class MatchContext():
     def __init__(self, context: IMatchContext):
         self.path_key = context['path_key']
         self.query = context['query']
+
+        self.__lifecyle_hooks = context.get('lifecycle_hooks') # Optional
         self.__response_param_names_facade = context['response_param_names_facade']
 
     def to_dict(self) -> IMatchContext:
         return {
+            'lifecycle_hooks': self.__lifecyle_hook,
             'path_key': self.path_key,
             'query': self.query,
             'response_param_names_facade': self.__response_param_names_facade,
@@ -54,6 +58,54 @@ class MatchContext():
         query: str = self.query
         required_param_names: ResponseParamName = response_param_names_facade.required
         return self.__param_name_matches(query, required_param_names)
+
+    ### Matchers
+
+    def length_matches(self, v1, v2):
+        handle_length_matches = self.__lifecyle_hook('handle_length_matches')
+
+        if handle_length_matches:
+            return handle_length_matches(self.clone(), v1, v2)
+        else:
+            return len(v1) == len(v2)
+
+    def param_name_exists(self, key, actual):
+        handle_param_name_exists = self.__lifecyle_hook('handle_param_name_exists')
+
+        if handle_param_name_exists:
+            return handle_param_name_exists(self.clone(), key, actual)
+        else:
+            return key in actual
+
+    def value_fuzzy_matches(self, v1, v2):
+        handle_fuzzy_matches = self.__lifecyle_hook('handle_fuzzy_matches')
+
+        if handle_fuzzy_matches:
+            return handle_fuzzy_matches(self.clone(), v1, v2)
+        else:
+            return type(v1) == type(v2)
+
+    def value_matches(self, v1, v2):
+        handle_value_matches = self.__lifecyle_hook('handle_value_matches')
+
+        if handle_value_matches:
+            return handle_value_matches(self.clone(), v1, v2)
+        else:
+            return v1 == v2
+
+    def value_type_exists(self, value, valid_types: list):
+        handle_type_exists = self.__lifecyle_hook('handle_type_exists')
+
+        if handle_type_exists:
+            return handle_type_exists(self.clone(), value, valid_types)
+        else:
+            return type(value) in valid_types
+
+    def __lifecyle_hook(self, name):
+        if not self.__lifecycle_hooks:
+            return
+
+        return self.lifecycle_hooks.get(name)
 
     def __param_name_matches(query, param_names: List[ResponseParamName]) -> bool:
         for param_name in param_names:
