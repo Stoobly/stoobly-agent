b9a51af59b49edfaf1c4411cef18639df53ec533
Jvlythical <lvlichael8@gmail.com>
2022-08-28 02:42:52 -0700

    Add support for test match hooks

--- stoobly_agent/app/proxy/test/iterable_matches.py
+++ stoobly_agent/app/proxy/test/iterable_matches.py
@@ -4,12 +4,11 @@ from typing import List, Tuple, TypedDict
 
 from .context import TestContext
 from .match_context import MatchContext
-from .response_param_names_facade import ResponseParamNamesFacade
 
 def dict_fuzzy_matches(context: TestContext, expected: dict, actual: dict):
-    response_param_names_facade: ResponseParamNamesFacade = context.response_param_names
-    match_context = MatchContext({ 'path_key': '', 'query': '', 'response_param_names_facade': response_param_names_facade })
-    return __dict_fuzzy_matches(expected, actual, match_context)
+    match_context = __initialize_match_context(context)
+
+    return __dict_fuzzy_matches(match_context, expected, actual)
 
 def __dict_fuzzy_matches(expected: dict, actual: dict, parent_context: MatchContext) -> Tuple[bool, str]:
     for key, expected_value in expected.items():
@@ -17,32 +16,32 @@ def __dict_fuzzy_matches(expected: dict, actual: dict, parent_context: MatchCont
         context.visit_dict(key)
         path_key = context.path_key
 
-        if not __param_name_exists(key, actual):
+        if not context.param_name_exists(key, actual):
             if not context.selected() or not context.required():
                 continue 
 
             return __param_name_exists_error(path_key)
 
         actual_value = actual[key]
-        if not __value_fuzzy_matches(actual_value, expected_value):
+        if not context.value_fuzzy_matches(actual_value, expected_value):
             if context.ignored(expected_value, actual_value):
                 continue
 
             return __type_match_error(path_key, expected_value, actual_value)
 
         if type(actual_value) is dict:
-            __dict_fuzzy_matches(expected_value, actual_value, context)
+            __dict_fuzzy_matches(context, expected_value, actual_value)
             continue
 
         if type(actual_value) is list:
-            __list_fuzzy_matches(expected_value, actual_value, context)
+            __list_fuzzy_matches(context, expected_value, actual_value)
             continue
 
     return True, ''
 
 def list_fuzzy_matches(context: TestContext, expected: dict, actual: dict):
-    response_param_names_facade: ResponseParamNamesFacade = context.response_param_names
-    match_context = MatchContext({ 'path_key': '', 'query': '', 'response_param_names_facade': response_param_names_facade })
+    match_context = __initialize_match_context(context)
+
     return __list_fuzzy_matches(expected, actual, match_context)
 
 ###
@@ -68,26 +67,26 @@ def __list_fuzzy_matches(expected: list, actual: list, parent_context: MatchCont
         context.visit_list(i)
         path_key = context.path_key
 
-        if not __value_type_exists(value, valid_types):
+        if not context.value_type_exists(value, valid_types):
             if not context.selected() or not context.required():
                 continue
 
             return __valid_type_error(path_key, value, valid_types)
 
         if type(value) is dict:
-            __dict_fuzzy_matches(type_examples[dict], value, context)
+            __dict_fuzzy_matches(context, type_examples[dict], value)
             continue
 
         if type(value) is list:
-            __list_fuzzy_matches(type_examples[list], value, context)
+            __list_fuzzy_matches(context, type_examples[list], value)
             continue
 
     return True, ''
 
 def dict_matches(context: TestContext, expected: dict, actual: dict) -> Tuple[bool, str]:
-    response_param_names_facade: ResponseParamNamesFacade = context.response_param_names
-    match_context = MatchContext({ 'path_key': '', 'query': '', 'response_param_names_facade': response_param_names_facade })
-    return __dict_matches(expected, actual, match_context)
+    match_context = __initialize_match_context(context)
+
+    return __dict_matches(match_context, expected, actual)
 
 def __dict_matches(expected: dict, actual: dict, parent_context: MatchContext) -> Tuple[bool, str]:
     for key, expected_value in expected.items():
@@ -96,7 +95,7 @@ def __dict_matches(expected: dict, actual: dict, parent_context: MatchContext) -
         path_key = context.path_key
 
         # Check if key exists in actual
-        if not __param_name_exists(key, actual):
+        if not context.param_name_exists(key, actual):
             if not context.selected() or not context.required():
                 continue
 
@@ -104,22 +103,22 @@ def __dict_matches(expected: dict, actual: dict, parent_context: MatchContext) -
 
         # Check if types match 
         actual_value = actual[key]
-        if not __value_fuzzy_matches(expected_value, actual_value):
+        if not context.value_fuzzy_matches(expected_value, actual_value):
             if context.ignored(expected_value, actual_value):
                 continue
 
             return __type_match_error(path_key, expected_value, actual_value)
 
         if type(actual_value) is dict:
-            __dict_matches(expected_value, actual_value, context)
+            __dict_matches(context, expected_value, actual_value)
             continue
 
         if type(actual_value) is list:
-            __list_matches(expected_value, actual_value, context)
+            __list_matches(context, expected_value, actual_value)
             continue
 
         # Check if value matches
-        if not __value_matches(expected_value, actual_value):
+        if not context.value_matches(expected_value, actual_value):
             if context.ignored(expected_value, actual_value) or not context.deterministic():
                 continue
 
@@ -128,12 +127,12 @@ def __dict_matches(expected: dict, actual: dict, parent_context: MatchContext) -
     return True, ''
 
 def list_matches(context: TestContext, expected: dict, actual: dict) -> Tuple[bool, str]:
-    response_param_names_facade: ResponseParamNamesFacade = context.response_param_names
-    match_context = MatchContext({ 'path_key': '', 'query': '', 'response_param_names_facade': response_param_names_facade })
-    return __list_matches(expected, actual, match_context)
+    match_context = __initialize_match_context(context)
+
+    return __list_matches(match_context, expected, actual)
 
 def __list_matches(expected: list, actual: list, parent_context: MatchContext) -> Tuple[bool, str]:
-    if not __length_matches(expected, actual):
+    if not context.length_matches(expected, actual):
         if parent_context.deterministic() and parent_context.selected():
             return __length_match_error(parent_context.path_key, expected, actual)
 
@@ -147,21 +146,21 @@ def __list_matches(expected: list, actual: list, parent_context: MatchContext) -
 
         actual_value = actual[i]
 
-        if not __value_fuzzy_matches(expected_value, actual_value):
+        if not context.value_fuzzy_matches(expected_value, actual_value):
             if context.ignored(expected_value, actual_value):
                 continue
 
             return __type_match_error(path_key, expected_value, actual_value)
 
         if type(actual_value) is dict:
-            __dict_matches(expected_value, actual_value, context)
+            __dict_matches(context, expected_value, actual_value)
             continue
 
         if type(actual_value) is list:
-            __list_matches(expected_value, actual_value, context)
+            __list_matches(context, expected_value, actual_value)
             continue
 
-        if not __value_matches(actual_value, expected_value):
+        if not context.value_matches(actual_value, expected_value):
             if context.ignored(expected_value, actual_value) or not context.deterministic():
                 continue
 
@@ -169,22 +168,13 @@ def __list_matches(expected: list, actual: list, parent_context: MatchContext) -
 
     return True, ''
 
-### Matchers
-
-def __length_matches(v1, v2):
-    return len(v1) == len(v2)
-
-def __param_name_exists(key, actual):
-    return key in actual
-
-def __value_fuzzy_matches(v1, v2):
-    return type(v1) == type(v2)
-
-def __value_matches(v1, v2):
-    return v1 == v2
-
-def __value_type_exists(value, valid_types: list):
-    return type(value) in valid_types
+def __initialize_match_context(context: TestContext):
+    return MatchContext({ 
+        'lifecycle_hooks': context.lifecycle_hooks,
+        'path_key': '', 
+        'query': '', 
+        'response_param_names_facade': context.response_param_names
+    })
 
 ### Match Errors
 
