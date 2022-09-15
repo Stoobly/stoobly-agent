b500f443952e6a28a5f217db12a528ae04c85f0b
Michael Yen <Michael Yen>
2022-09-15 01:08:34 -0700

    Move app/proxy/test helpers

---
+++ stoobly_agent/app/proxy/test/helpers/endpoint_facade.py
@@ -0,0 +1,74 @@
+
+from stoobly_agent.app.proxy.test.helpers.request_component_names_facade import RequestComponentNamesFacade
+from stoobly_agent.config.constants import test_filter
+from stoobly_agent.lib.api.endpoints_resource import EndpointsResource
+from stoobly_agent.lib.api.interfaces.endpoints import EndpointShowResponse
+
+class EndpointFacade():
+
+  def __init__(self, endpoints_resource: EndpointsResource, endpoint_id: int):
+    self.__endpoints_resource = endpoints_resource
+    self.__endpoint_id = endpoint_id
+    
+    self.trace = None
+    self.filter = test_filter.ALL
+
+    # Cache
+    self.__cached_endpoint_show_response = None
+    self.__cached_header_names = None
+    self.__cached_response_param_names = None
+
+  def get(self) -> EndpointShowResponse:
+    if self.__cached_endpoint_show_response:
+      return self.__cached_endpoint_show_response
+
+    endpoint_id = self.__endpoint_id
+    resource = self.__endpoints_resource
+
+    if not resource or not endpoint_id:
+      return 
+
+    try:
+      res = resource.show(endpoint_id, aliases=True, response_param_names=True)
+    except Exception as e:
+      return 
+
+    if not res.ok:
+      return 
+
+    self.__cached_endpoint_show_response: EndpointShowResponse = res.json()
+
+    return self.__cached_endpoint_show_response
+
+  @property
+  def aliases(self):
+    endpoint_show_response: EndpointShowResponse = self.get()
+    return endpoint_show_response['aliases']
+
+  @property
+  def header_names(self) -> RequestComponentNamesFacade:
+    if self.__cached_header_names:
+      return self.__cached_header_names
+
+    self.__cached_header_names = self.__request_component_names('header_names')
+
+    return self.__cached_header_names
+
+  @property
+  def response_param_names(self) -> RequestComponentNamesFacade:
+    if self.__cached_response_param_names:
+      return self.__cached_response_param_names
+
+    self.__cached_response_param_names = self.__request_component_names('response_param_names')
+
+    return self.__cached_response_param_names
+
+  def __request_component_names(self, component_name):
+    endpoint_show_response: EndpointShowResponse = self.get()
+    aliases = self.aliases
+
+    component_names = endpoint_show_response[component_name]
+
+    return RequestComponentNamesFacade(
+      component_names      
+    ).with_aliases(aliases).with_filter(self.filter).with_trace(self.trace)
--- stoobly_agent/app/proxy/test/helpers/stoobly_response_adapter.py
+++ stoobly_agent/app/proxy/test/helpers/stoobly_response_adapter.py
@@ -2,7 +2,7 @@ from urllib.request import Request
 from stoobly_agent.lib.api.interfaces.request_show_response import RequestShowResponse
 from stoobly_agent.lib.api.interfaces.response_show_response import ResponseShowResponse
 
-from .context_response import TestContextResponse
+from ..context_response import TestContextResponse
 
 class StooblyResponseAdapter():
 
