b9c6889f3cd79926f90b656ac3b955b81c7ee933
Michael Yen <Michael Yen>
2022-09-18 14:02:27 -0700

    Update tests for matching

---
+++ stoobly_agent/test/app/test/contract_matches_test.py
@@ -0,0 +1,89 @@
+import pdb
+import pytest
+
+from stoobly_agent.app.proxy.test.contract_matches import dict_matches, list_matches
+from stoobly_agent.app.proxy.test.helpers.request_component_names_facade import RequestComponentNamesFacade
+from stoobly_agent.app.proxy.test.match_context import MatchContext
+from stoobly_agent.test.mock_data.endpoint_show_response import endpoint_show_response
+
+@pytest.fixture
+def endpoints_list_response_param_names_facade():
+  response_param_names = endpoint_show_response['response_param_names']
+  aliases = endpoint_show_response['aliases']
+  return RequestComponentNamesFacade(response_param_names).with_aliases(aliases)
+
+class TestListMatchesList():
+
+  def test_matches(self, endpoints_list_response_param_names_facade: RequestComponentNamesFacade):
+    matches, log = list_matches(
+      MatchContext({
+        'path_key': '', 'query': '', 'request_component_names_facade': endpoints_list_response_param_names_facade
+      }),
+      [{
+        "id": 934,
+        "requests_count": 2,
+        "category": 2,
+        "path": "/users/:userId",
+        "method": "GET",
+        "created_at": "2022-05-31T06:00:10.663Z",
+        "updated_at": "2022-05-31T06:00:10.663Z",
+        "components": [],
+        "url": "https://alpha.api.stoobly.com/endpoints/934"
+      }]
+    )
+    assert matches, log
+
+  def test_not_matches_list_type(self, endpoints_list_response_param_names_facade: RequestComponentNamesFacade):
+    matches, log = list_matches(
+      MatchContext({
+        'path_key': '', 'query': '', 'request_component_names_facade': endpoints_list_response_param_names_facade
+      }),
+      [1]
+    )
+
+    assert log == "Key '[0]' type did not match: got <class 'int'>, expected valid types Hash", log
+
+  def test_not_matches_list_element_type(self, endpoints_list_response_param_names_facade: RequestComponentNamesFacade):
+    match_context = MatchContext({
+      'path_key': '', 'query': '', 'request_component_names_facade': endpoints_list_response_param_names_facade
+    })
+
+    matches, log = list_matches(
+      match_context, [{ "id": "934" }]
+    )
+
+    assert log == "Key '[0].id' type did not match: got <class 'str'>, expected valid types Integer", log
+
+  def test_not_matches_list_properties(self, endpoints_list_response_param_names_facade: RequestComponentNamesFacade):
+    match_context = MatchContext({
+      'path_key': '', 'query': '', 'request_component_names_facade': endpoints_list_response_param_names_facade
+    })
+
+    matches, log = list_matches(
+      match_context, [{ "id": 934 }]
+    )
+
+    assert log == "Missing key: expected [0].requests_count to exist", log
+
+
+  def test_not_matches_list_properties(self, endpoints_list_response_param_names_facade: RequestComponentNamesFacade):
+    match_context = MatchContext({
+      'path_key': '', 'query': '', 'request_component_names_facade': endpoints_list_response_param_names_facade
+    })
+
+    matches, log = list_matches(
+      match_context, []
+    )
+
+    assert log == "Key '[*]' length did not match: got 0", log
+
+  def test_not_matches_list(self, endpoints_list_response_param_names_facade: RequestComponentNamesFacade):
+    match_context = MatchContext({
+      'path_key': '', 'query': '', 'request_component_names_facade': endpoints_list_response_param_names_facade
+    })
+
+    matches, log = list_matches(
+      match_context, {}
+    )
+
+    assert log == "Key '[*]' type did not match: got <class 'dict'>, expected <class 'list'>", log
---
+++ stoobly_agent/test/app/test/fuzzy_matches_test.py
@@ -0,0 +1,94 @@
+import pytest
+
+from stoobly_agent.app.proxy.test.fuzzy_matches import list_fuzzy_matches
+from stoobly_agent.app.proxy.test.helpers.request_component_names_facade import RequestComponentNamesFacade
+from stoobly_agent.app.proxy.test.match_context import MatchContext
+from stoobly_agent.test.mock_data.endpoint_show_response import endpoint_show_response
+
+@pytest.fixture
+def endpoints_list_response_param_names_facade():
+  response_param_names = endpoint_show_response['response_param_names']
+  aliases = endpoint_show_response['aliases']
+  return RequestComponentNamesFacade(response_param_names).with_aliases(aliases)
+  
+@pytest.fixture
+def empty_response_param_names_facade():
+  response_param_names = []
+  return RequestComponentNamesFacade(response_param_names)
+
+class TestMatchesFuzzySanity():
+  def test_matches_empty_response(self, endpoints_list_response_param_names_facade):
+    matches, log = list_fuzzy_matches(
+      MatchContext({
+        'path_key': '', 'query': '', 'request_component_names_facade': endpoints_list_response_param_names_facade
+      }),
+      [], []
+    )
+    assert matches, log
+
+  def test_matches_list_of_numbers(self, empty_response_param_names_facade):
+    matches, log = list_fuzzy_matches(
+      MatchContext({
+        'path_key': '', 'query': '', 'request_component_names_facade': empty_response_param_names_facade
+      }),
+      [1], [2]
+    )
+    assert matches, log
+
+  def test_not_matches_mixed(self, endpoints_list_response_param_names_facade):
+    matches, log = list_fuzzy_matches(
+      MatchContext({
+        'path_key': '', 'query': '', 'request_component_names_facade': endpoints_list_response_param_names_facade
+      }),
+      [{}], [{}, 1]
+    )
+    
+    assert log == "Key '[1]' type did not match: got <class 'int'>, expected valid types <class 'dict'>", log 
+
+  def test_not_matches(self, endpoints_list_response_param_names_facade):
+    matches, log = list_fuzzy_matches(
+      MatchContext({
+        'path_key': '', 'query': '', 'request_component_names_facade': endpoints_list_response_param_names_facade
+      }),
+      [], {}
+    )
+    assert log == "Key '' type did not match: got <class 'dict'>, expected <class 'list'>", log 
+
+class TestFuzzyMatchesListOfDicts():
+
+  def test_matches_response_one_object(self, endpoints_list_response_param_names_facade):
+    actual = [
+      {
+        "id": 1000,
+        "requests_count": 8,
+        "category": 2,
+        "path": "/abc",
+        "method": "POST",
+        "created_at": "2022-05-26T23:46:49.968Z",
+        "updated_at": "2022-05-26T23:46:49.968Z",
+        "components": [],
+        "url": "https://alpha.api.stoobly.com/endpoints/930"
+      },
+    ]
+
+    expected = [
+      {
+        "id": 930,
+        "requests_count": 6,
+        "category": 1,
+        "path": "/abc",
+        "method": "GET",
+        "created_at": "2022-05-26T23:46:49.968Z",
+        "updated_at": "2022-05-26T23:46:49.968Z",
+        "components": [],
+        "url": "https://alpha.api.stoobly.com/endpoints/930"
+      },
+    ] 
+
+    matches, log = list_fuzzy_matches(
+      MatchContext({
+        'path_key': '', 'query': '', 'request_component_names_facade': endpoints_list_response_param_names_facade
+      }),
+      expected, actual
+    )
+    assert matches, log
