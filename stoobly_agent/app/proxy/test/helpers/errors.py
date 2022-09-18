99134169cf41c312c053f9e521deda210cf8d21e
jvlythical <lvlichael8@gmail.com>
2022-09-18 14:08:34 -0700

    Update test errors

--- stoobly_agent/app/proxy/test/helpers/errors.py
+++ stoobly_agent/app/proxy/test/helpers/errors.py
@@ -1,5 +1,10 @@
+import pdb
+
 def length_match_error(path_key, expected_value, actual_value):
-    return False, f"Key '{path_key}' length did not match: got {len(actual_value)}, expected {len(expected_value)}"
+    if not expected_value:
+        return False, f"Key '{path_key}' length did not match: got {len(actual_value)}"
+    else:
+        return False, f"Key '{path_key}' length did not match: got {len(actual_value)}, expected {len(expected_value)}"
 
 def param_name_exists_error(path_key):
     return False, f"Extra key: expected {path_key} to not exist"
@@ -8,9 +13,10 @@ def param_name_missing_error(path_key):
     return False, f"Missing key: expected {path_key} to exist"
 
 def type_match_error(path_key, expected_value, actual_value):
-    return False, f"Key '{path_key}' type did not match: got {type(actual_value)}, expected {type(expected_value)}"
+    return False, f"Key '{path_key}' type did not match: got {actual_value}, expected {expected_value}"
 
 def valid_type_error(path_key, value, valid_types):
+    valid_types = list(map(lambda t: str(t), valid_types))
     return False, f"Key '{path_key}' type did not match: got {type(value)}, expected valid types {', '.join(valid_types)}"
 
 def value_match_error(path_key, expected_value, actual_value):
