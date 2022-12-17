dec16479822261da442807af0b0e444677713464
jvlythical <lvlichael8@gmail.com>
2022-12-17 01:36:21 -0800

    Add query params controller

---
+++ stoobly_agent/app/models/adapters/query_param_adapter_factory.py
@@ -0,0 +1,12 @@
+from stoobly_agent.app.settings import RemoteSettings
+from stoobly_agent.lib.orm.request import Request
+
+from .local_db.query_param_adapter import LocalDBQueryParamAdapter
+
+class QueryParamAdapterFactory():
+
+  def __init__(self, settings: RemoteSettings):
+    self.__remote_settings = settings
+
+  def local_db(self) -> LocalDBQueryParamAdapter:
+    return LocalDBQueryParamAdapter(Request)  
