c5bccef0e495ee7ad01d24d83195f12293bf8a6d
jvlythical <lvlichael8@gmail.com>
2022-12-24 02:38:39 -0800

    Update RequestStringControl to be more flexible

--- stoobly_agent/app/proxy/upload/request_string_control.py
+++ stoobly_agent/app/proxy/upload/request_string_control.py
@@ -1,10 +1,15 @@
+from typing import Union
+
 class RequestStringControl():
   REQUEST_TYPE = 1
 
-  def __init__(self):
-    self.__request_type = self.REQUEST_TYPE
-    self.__id = None
-    self.__timestamp = None
+  def __init__(self, c: Union[bytes, str] = None):
+    if not c:
+      self.__request_type = self.REQUEST_TYPE
+      self.__id = None
+      self.__timestamp = None
+    else:
+      self.parse(c)
 
   @property
   def id(self):
@@ -26,7 +31,10 @@ class RequestStringControl():
   def timestamp(self, t: int):
     self.__timestamp = t
 
-  def parse(self, s: str):
+  def parse(self, s: Union[bytes, str]):
+    if isinstance(s, bytes):
+      s = s.decode()
+
     toks = s.split(' ')
     self.__id = toks[1]
     self.__timestamp = int(toks[2])
