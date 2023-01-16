0c713dd2a43f7c549bd5b536c180df2888c706b3
Michael Yen <Michael Yen>
2023-01-16 15:52:31 -0800

    Fix bugs

--- stoobly_agent/app/api/responses_controller.py
+++ stoobly_agent/app/api/responses_controller.py
@@ -41,12 +41,12 @@ class ResponsesController:
 
         accepted_headers = ['content-encoding', 'content-length', 'content-type']
         for header, val in response.headers.items():
-            decoded_header = header.decode().lower()
+            decoded_header = header.lower()
 
             if decoded_header not in accepted_headers:
                 continue 
 
-            headers[decoded_header] = val.decode()
+            headers[decoded_header] = val
 
         context.render(
             data = response.content,
