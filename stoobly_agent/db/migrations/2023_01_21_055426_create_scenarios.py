1b8601cdf40d0a90ece9fda063eeb1f0835873f3
Michael Yen <Michael Yen>
2023-01-24 23:43:42 -0800

    Rename create_scenarios migration

--- stoobly_agent/db/migrations/2023_01_21_055426_create_scenarios.py
+++ stoobly_agent/db/migrations/2023_01_21_055426_create_scenarios.py
@@ -9,11 +9,12 @@ class CreateScenarios(Migration):
         with self.schema.create('scenarios') as table:
             table.increments('id')
             table.string('name')
-            table.integer('request_count').default(0)
+            table.integer('requests_count').default(0)
             table.string('description').default("")
             table.boolean('starred').default(False)
             table.integer('priority').default(0)
             table.boolean('is_deleted').default(False)
+            table.timestamps()
 
     def down(self):
         """
