262c28a04b9d040c2c51acea52b492a64367a7fb
jvlythical <lvlichael8@gmail.com>
2023-02-09 23:09:53 -0800

    Add request relationship to Response

--- stoobly_agent/lib/orm/response.py
+++ stoobly_agent/lib/orm/response.py
@@ -1,4 +1,12 @@
+from orator.orm import belongs_to
+
+from stoobly_agent.app.models.schemas.request import Request
+
 from .base import Base
 
 class Response(Base):
   __fillable__ = ['control','request_id', 'raw']
+
+  @belongs_to
+  def request(self):
+    return Request
