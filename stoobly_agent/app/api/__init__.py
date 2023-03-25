1fa42e6a4cd1da0f718c6733c25db48a9f0ab804
jvlythical <lvlichael8@gmail.com>
2023-03-25 16:28:12 -0700

    Update api init to set url in settings

--- stoobly_agent/app/api/__init__.py
+++ stoobly_agent/app/api/__init__.py
@@ -4,6 +4,7 @@ import threading
 
 from http.server import HTTPServer
 
+from stoobly_agent.app.settings import Settings
 from stoobly_agent.config.constants import env_vars
 
 from .application_http_request_handler import ApplicationHTTPRequestHandler
@@ -15,8 +16,13 @@ def start_server(host, port):
 def run(**kwargs):
     ui_host = kwargs['ui_host']
     ui_port = kwargs['ui_port']
+    url = f"http://{kwargs['ui_host']}:{kwargs['ui_port']}"
 
-    os.environ[env_vars.AGENT_URL] = f"http://{kwargs['ui_host']}:{kwargs['ui_port']}"
+    os.environ[env_vars.AGENT_URL] = url
+
+    settings = Settings.instance()
+    settings.ui.url = url
+    settings.commit()
 
     print(f"UI server listening at http://{ui_host}:{ui_port}\n")
 
