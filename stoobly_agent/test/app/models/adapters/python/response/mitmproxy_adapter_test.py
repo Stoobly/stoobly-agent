e52f88471e246a16fd22745bd53d9b61a0fc70d8
jvlythical <lvlichael8@gmail.com>
2023-04-24 21:54:02 -0700

    Add tests for app/models/adapters/python/response

---
+++ stoobly_agent/test/app/models/adapters/python/response/mitmproxy_adapter_test.py
@@ -0,0 +1,31 @@
+import pdb
+import requests
+
+from mitmproxy.http import Response as MitmproxyResponse
+
+from stoobly_agent.test.test_helper import DETERMINISTIC_GET_REQUEST_URL, NON_DETERMINISTIC_GET_REQUEST_URL
+
+from stoobly_agent.app.models.adapters.python import PythonResponseAdapterFactory
+
+class TestMitmproxyAdapter():
+
+  def test_deterministic_request(self):
+    res = requests.get(DETERMINISTIC_GET_REQUEST_URL) 
+
+    mitmproxy_response = PythonResponseAdapterFactory(res).mitmproxy_response()
+    self.__test_equivalence(mitmproxy_response, res)
+
+  def test_non_deterministic_request(self):
+    res = requests.get(NON_DETERMINISTIC_GET_REQUEST_URL) 
+
+    mitmproxy_response = PythonResponseAdapterFactory(res).mitmproxy_response()
+    self.__test_equivalence(mitmproxy_response, res)
+
+  def __test_equivalence(self, mitmproxy_response: MitmproxyResponse, response: requests.Response):
+    assert mitmproxy_response.raw_content == response.raw.data
+    assert mitmproxy_response.content == response.content
+    assert mitmproxy_response.status_code == response.status_code
+
+    for key, val in response.headers.items():
+      assert val == mitmproxy_response.headers.get(key)
+
---
+++ stoobly_agent/test/app/models/adapters/python/response/raw_adapter_test.py
@@ -0,0 +1,33 @@
+import pdb
+import requests
+
+from stoobly_agent.test.test_helper import DETERMINISTIC_GET_REQUEST_URL, NON_DETERMINISTIC_GET_REQUEST_URL
+
+from stoobly_agent.app.models.adapters.python import PythonResponseAdapterFactory
+from stoobly_agent.app.models.adapters.raw_http_response_adapter import RawHttpResponseAdapter
+
+class TestMitmproxyAdapter():
+
+  def test_deterministic_request(self):
+    res = requests.get(DETERMINISTIC_GET_REQUEST_URL, stream=True) 
+
+    raw_response = PythonResponseAdapterFactory(res).raw_response()
+    self.__test_equivalence(raw_response, res)
+
+  def test_non_deterministic_request(self):
+    res = requests.get(NON_DETERMINISTIC_GET_REQUEST_URL, stream=True) 
+
+    raw_response = PythonResponseAdapterFactory(res).raw_response()
+    self.__test_equivalence(raw_response, res)
+
+  def __test_equivalence(self, raw_response: str, response: requests.Response):
+    _response = RawHttpResponseAdapter(raw_response).to_response()
+    
+    assert _response.request == None
+    assert _response.raw.data == response.raw.data
+    assert _response.status_code == response.status_code
+
+    for key, val in response.headers.items():
+      assert val == _response.headers.get(key)
+
+
