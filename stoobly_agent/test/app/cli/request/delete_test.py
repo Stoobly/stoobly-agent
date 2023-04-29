009a2d06a298f654f501365cd2a4b17183970c6a
jvlythical <lvlichael8@gmail.com>
2023-04-28 23:02:27 -0700

    Add test for request delete

---
+++ stoobly_agent/test/app/cli/request/delete_test.py
@@ -0,0 +1,48 @@
+import pdb
+import pytest
+
+from click.testing import CliRunner
+
+from stoobly_agent.test.test_helper import DETERMINISTIC_GET_REQUEST_URL, reset
+
+from stoobly_agent.cli import record, request
+from stoobly_agent.lib.api.keys import RequestKey
+from stoobly_agent.lib.orm.request import Request
+
+@pytest.fixture(scope='module')
+def runner():
+      return CliRunner()
+
+@pytest.fixture(autouse=True, scope='module')
+def settings():
+  return reset()
+
+class TestDelete():
+
+  @pytest.fixture(autouse=True, scope='class')
+  def request_create_cli_result(self, runner: CliRunner):
+    record_result = runner.invoke(record, [DETERMINISTIC_GET_REQUEST_URL])
+    assert record_result.exit_code == 0
+    return record_result
+
+  @pytest.fixture(autouse=True, scope='class')
+  def request_delete_cli_results(self, runner: CliRunner):
+    _request = Request.last()
+    request_delete_result = runner.invoke(request, ['delete', _request.key()])
+    assert request_delete_result.exit_code == 0
+
+    return request_delete_result
+
+  def test_it_sets_is_deleted(self):
+    request = Request.last()
+    assert request and request.is_deleted
+
+  class TestWhenDeletingAgain():
+
+    def test_it_hard_deletes(self, runner):
+      _request = Request.last()
+      request_delete_result = runner.invoke(request, ['delete', _request.key()])
+      assert request_delete_result.exit_code == 0
+
+      assert Request.last() == None
+
