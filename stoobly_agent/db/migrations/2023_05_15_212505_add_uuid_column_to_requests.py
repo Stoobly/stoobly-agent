7c1213893a0499d6d344af57f7d7010102ab7294
jvlythical <lvlichael8@gmail.com>
2023-05-20 18:56:18 -0700

    Add uuid columns to request and scenario tables

---
+++ stoobly_agent/db/migrations/2023_05_15_212505_add_uuid_column_to_requests.py
@@ -0,0 +1,30 @@
+import pdb
+import uuid
+
+from orator.migrations import Migration
+
+from stoobly_agent.app.proxy.record.request_string_control import RequestStringControl
+from stoobly_agent.lib.orm.request import Request
+
+class AddUuidColumnToRequests(Migration):
+
+    def up(self):
+        """
+        Run the migrations.
+        """
+        with self.schema.table('requests') as table:
+            table.string('uuid', 36).default('').index()
+
+        for request in Request.all():
+            control = RequestStringControl(request.control)
+            try:
+                request.update(uuid=str(uuid.UUID(control.id)))
+            except Exception:
+                request.update(uuid=str(uuid.uuid4()))
+
+    def down(self):
+        """
+        Revert the migrations.
+        """
+        with self.schema.table('requests') as table:
+            table.drop_column('uuid')
---
+++ stoobly_agent/db/migrations/2023_05_15_213119_add_uuid_column_to_scenarios.py
@@ -0,0 +1,24 @@
+import uuid
+
+from orator.migrations import Migration
+
+from stoobly_agent.lib.orm.scenario import Scenario
+
+class AddUuidColumnToScenarios(Migration):
+
+    def up(self):
+        """
+        Run the migrations.
+        """
+        with self.schema.table('scenarios') as table:
+            table.string('uuid', 36).index().default('')
+
+        for scenario in Scenario.all():
+            scenario.update(uuid=str(uuid.uuid4()))
+
+    def down(self):
+        """
+        Revert the migrations.
+        """
+        with self.schema.table('scenarios') as table:
+            table.drop_column('uuid')
