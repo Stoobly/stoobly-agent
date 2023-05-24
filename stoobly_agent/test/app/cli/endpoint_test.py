64b5c1fe36d550c8bc305e0c29e8cf66b80541cd
stoobmmelier <87163625+stoobmmelier@users.noreply.github.com>
2023-05-23 22:45:38 -0700

    Clean up rough drafts

--- stoobly_agent/test/app/cli/endpoint_test.py
+++ stoobly_agent/test/app/cli/endpoint_test.py
@@ -3,6 +3,7 @@ import pytest
 from stoobly_agent.app.cli.helpers.endpoint_facade import EndpointFacade
 
 from stoobly_agent.app.settings import Settings
+from stoobly_agent.app.models.types import EndpointCreateParams, OPENAPI_FORMAT
 
 
 @pytest.fixture
@@ -16,8 +17,12 @@ def petstore_expanded_file_path():
   return path 
 
 class TestEndpointFacade():
-  def test_main_foo(self, petstore_expanded_file_path):
+  def test_create(self, petstore_expanded_file_path):
     settings = Settings.instance()
-    open_api_facade = EndpointFacade(settings=settings)
+    open_api_facade = EndpointFacade(settings)
+    params: EndpointCreateParams = {}
+    params['format'] = OPENAPI_FORMAT
+    params['endpoints'] = petstore_expanded_file_path
 
-    open_api_facade.main_foo(petstore_expanded_file_path)
+    open_api_facade.create(**params)
+
