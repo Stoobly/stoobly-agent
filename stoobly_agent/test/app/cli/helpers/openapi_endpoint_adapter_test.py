f7845a3d56ec407042dc50044eb901d9d643c76c
stoobmmelier <87163625+stoobmmelier@users.noreply.github.com>
2023-05-28 03:29:45 -0700

    Enhance adapter test

--- stoobly_agent/test/app/cli/helpers/openapi_endpoint_adapter_test.py
+++ stoobly_agent/test/app/cli/helpers/openapi_endpoint_adapter_test.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-from pprint import pprint
 import pdb
+from pprint import pprint
+from typing import Dict
 
 import pytest
 
@@ -8,19 +9,117 @@ from stoobly_agent.app.cli.helpers.openapi_endpoint_adapter import (
   OpenApiEndpointAdapter,
 )
 
-@pytest.fixture
+@pytest.fixture(scope='class')
 def mock_data_directory_path():
   return Path(__file__).parent.parent.parent.parent / 'mock_data'
 
-@pytest.fixture
+@pytest.fixture(scope='class')
 def petstore_file_path(mock_data_directory_path):
   path = mock_data_directory_path / "petstore.yaml"
   return path 
 
-@pytest.fixture
+@pytest.fixture(scope='class')
 def petstore_expanded_file_path(mock_data_directory_path):
   path = mock_data_directory_path / "petstore-expanded.yaml"
-  return path 
+  return path
+
+@pytest.fixture(scope='class')
+def expected_get_pets_endpoint() -> Dict:
+  return {
+    'id': 1,
+    'method': 'GET',
+    'host': 'petstore.swagger.io',
+    'port': '443',
+    'match_pattern': '/v2/pets',
+    'path': '/v2/pets',
+    'query_param_names': [
+      {
+        'endpoint_id': 1,
+        'id': 1,
+        'inferred_type': 'Array',
+        'is_deterministic': True,
+        'is_required': False,
+        'name': 'tags',
+        'query': 'tags',
+        'query_param_name_id': None},
+      {
+        'endpoint_id': 1,
+        'id': 2,
+        'inferred_type': 'String',
+        'is_required': False,
+        'name': 'TagsElement',
+        'query': 'tags[*]',
+        'query_param_name_id': 1
+      },
+      {
+        'endpoint_id': 1,
+        'id': 3,
+        'inferred_type': 'Integer',
+        'is_deterministic': True,
+        'is_required': False,
+        'name': 'limit',
+        'query': 'limit',
+        'query_param_name_id': None
+      }
+    ]
+  }
+
+@pytest.fixture(scope='class')
+def expected_post_pets_endpoint() -> Dict:
+  return {
+    'id': 2,
+    'method': 'POST',
+    'host': 'petstore.swagger.io',
+    'port': '443',
+    'match_pattern': '/v2/pets',
+    'path': '/v2/pets',
+    'body_param_names': [
+      {
+        'body_param_name_id': None,
+        'endpoint_id': 2,
+        'id': 1,
+        'inferred_type': 'String',
+        'is_deterministic': True,
+        'is_required': True,
+        'name': 'name',
+        'query': 'name'
+      },
+      {
+        'body_param_name_id': None,
+        'endpoint_id': 2,
+        'id': 2,
+        'inferred_type': 'String',
+        'is_deterministic': True,
+        'is_required': False,
+        'name': 'tag',
+        'query': 'tag'
+      },
+    ],
+  }
+
+@pytest.fixture(scope='class')
+def expected_get_pets_id_endpoint() -> Dict:
+  return {
+    'id': 3,
+    'method': 'GET',
+    'host': 'petstore.swagger.io',
+    'port': '443',
+    'path': '/v2/pets/{id}',
+    'match_pattern': '/v2/pets/%',
+    'aliases': [{'id': 1, 'name': '{id}'}],
+  }
+
+@pytest.fixture(scope='class')
+def expected_delete_pets_id_endpoint() -> Dict:
+  return {
+    'id': 4,
+    'method': 'DELETE',
+    'host': 'petstore.swagger.io',
+    'path': '/v2/pets/{id}',
+    'match_pattern': '/v2/pets/%',
+    'port': '443',
+    'aliases': [{'id': 1, 'name': '{id}'}],
+  }
 
 @pytest.fixture(scope='class')
 def open_api_endpoint_adapter():
@@ -28,12 +127,22 @@ def open_api_endpoint_adapter():
   return adapter
 
 class TestOpenApiEndpointAdapter():
-  def test_create(self, open_api_endpoint_adapter, petstore_expanded_file_path):
+  def test_create(self, open_api_endpoint_adapter, petstore_expanded_file_path, expected_get_pets_endpoint, expected_post_pets_endpoint, expected_get_pets_id_endpoint, expected_delete_pets_id_endpoint):
     adapter = open_api_endpoint_adapter
     file_path = petstore_expanded_file_path
 
     endpoints = adapter.adapt_from_file(file_path)
 
-    # pdb.set_trace()
     assert len(endpoints) == 4
+
+    get_pets_endpoint = endpoints[0]
+    post_gets_endpoint = endpoints[1]
+    get_pets_id_endpoint = endpoints[2]
+    delete_pets_id_endpoint = endpoints[3]
+    # pdb.set_trace()
+
+    assert get_pets_endpoint == expected_get_pets_endpoint
+    assert post_gets_endpoint == expected_post_pets_endpoint
+    assert get_pets_id_endpoint == expected_get_pets_id_endpoint
+    assert delete_pets_id_endpoint == expected_delete_pets_id_endpoint
 
