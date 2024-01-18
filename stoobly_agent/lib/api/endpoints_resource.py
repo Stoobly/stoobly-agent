1e9b3a733067399ea46d26be722cd4f9b80423d2
jvlythical <lvlichael8@gmail.com>
2024-01-18 00:51:13 -0800

    Add create method to EndpointsResource

--- stoobly_agent/lib/api/endpoints_resource.py
+++ stoobly_agent/lib/api/endpoints_resource.py
@@ -7,17 +7,21 @@ from .interfaces import EndpointsIndexQueryParams
 from .stoobly_api import StooblyApi
 
 class EndpointsResource(StooblyApi):
-  PROJECTS_ENDPOINT = 'endpoints'
+  ENDPOINTS_ENDPOINT = 'endpoints'
+
+  def create(self, **params):
+    url = f"{self.service_url}/{self.ENDPOINTS_ENDPOINT}"
+    return self.post(url, headers=self.default_headers, data=params)
 
   def index(self, **query_params: EndpointsIndexQueryParams) -> requests.Response:
-    url = f"{self.service_url}/{self.PROJECTS_ENDPOINT}"
+    url = f"{self.service_url}/{self.ENDPOINTS_ENDPOINT}"
 
     Logger.instance().debug(f"{self.LOG_ID}.request_response:{url}?{urllib.parse.urlencode(query_params)}")
 
     return self.get(url, headers=self.default_headers, params=query_params)
 
   def show(self, endpoint_id: int, **query_params) -> requests.Response:
-    url = f"{self.service_url}/{self.PROJECTS_ENDPOINT}/{endpoint_id}"
+    url = f"{self.service_url}/{self.ENDPOINTS_ENDPOINT}/{endpoint_id}"
 
     Logger.instance().debug(f"{self.LOG_ID}.request_response:{url}?{urllib.parse.urlencode(query_params)}")
 
