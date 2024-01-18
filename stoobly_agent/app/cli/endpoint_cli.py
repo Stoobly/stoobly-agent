8c5ea53e04a4fb419628c8156546dd99e84770e8
jvlythical <lvlichael8@gmail.com>
2024-01-18 01:30:16 -0800

    Add endpoint apply command

--- stoobly_agent/app/cli/endpoint_cli.py
+++ stoobly_agent/app/cli/endpoint_cli.py
@@ -1,12 +1,16 @@
+import sys
 import traceback
 
 import click
 
 from stoobly_agent.app.models.types import OPENAPI_FORMAT
 from stoobly_agent.app.settings import Settings
+from stoobly_agent.lib.logger import bcolors
 from stoobly_agent.lib.utils.conditional_decorator import ConditionalDecorator
 
 from .helpers.endpoint_facade import EndpointFacade
+from .helpers.endpoints_apply_service import apply_endpoints
+from .helpers.validations import validate_project_key
 from .types.endpoint import EndpointCreateCliOptions
 
 settings = Settings.instance()
@@ -41,3 +45,26 @@ def _import(**kwargs: EndpointCreateCliOptions):
     print("Failed to import API specification, stack trace:\n")
     print(traceback.format_exc())
 
+@endpoint.command(
+  help="Apply endpoint to requests"
+)
+@click.option('--remote-project-key', help='Which remote project to apply endpoints from.')
+def apply(**kwargs):
+  project_key = kwargs.get('remote_project_key')
+
+  if not project_key:
+    project_key = settings.remote.project_key
+
+  if not project_key:
+    print('Error: --remote-project-key not set', file=sys.stderr)
+    sys.exit(1)
+
+  project_key = validate_project_key(project_key)
+  endpoint_handler = lambda endpoint: print(f"{bcolors.OKBLUE}Applying Endpoint: {endpoint['path']}{bcolors.ENDC}")
+  request_handler = lambda request: print(request.url)
+
+  apply_endpoints(
+    project_key.id,
+    endpoint_handler=endpoint_handler,
+    request_handler=request_handler
+  )
