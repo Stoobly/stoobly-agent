9c30b19753b82f642c94f6d46d849335b7a5c739
jvlythical <lvlichael8@gmail.com>
2024-02-16 00:45:23 -0800

    Rename TestResultsBuilder in test

--- stoobly_agent/test/app/proxy/test/helpers/test_results_builder_test.py
+++ stoobly_agent/test/app/proxy/test/helpers/test_results_builder_test.py
@@ -1,10 +1,10 @@
 import pdb
 import pytest
 
-from stoobly_agent.app.proxy.test.helpers.test_results_builder import DownloadTestData, TestResultsBuilder
+from stoobly_agent.app.proxy.test.helpers.test_results_builder import DownloadTestData, TestResultsBuilder as Builder
 from stoobly_agent.config.constants import test_strategy
 
-class TestTestResultsBuilder():
+class TestBuilder():
 
   class TestWhenValid():
     @pytest.fixture(scope='class')
@@ -21,34 +21,34 @@ class TestTestResultsBuilder():
       }
 
     @pytest.fixture(scope='class')
-    def builder(self, test_results) -> TestResultsBuilder:
-      _builder = TestResultsBuilder(**test_results)
-      builder = TestResultsBuilder()
+    def builder(self, test_results) -> Builder:
+      _builder = Builder(**test_results)
+      builder = Builder()
       builder.deserialize(_builder.serialize())
       return builder
 
-    def test_it_has_expected_response(self, builder: TestResultsBuilder, test_results: DownloadTestData):
+    def test_it_has_expected_response(self, builder: Builder, test_results: DownloadTestData):
       assert builder.expected_response == test_results['expected_response']
 
-    def test_it_has_log(self, builder: TestResultsBuilder, test_results: DownloadTestData):
+    def test_it_has_log(self, builder: Builder, test_results: DownloadTestData):
       assert builder.log == test_results['log']
 
-    def test_it_has_passed(self, builder: TestResultsBuilder, test_results: DownloadTestData):
+    def test_it_has_passed(self, builder: Builder, test_results: DownloadTestData):
       assert builder.passed == test_results['passed']
 
-    def test_it_has_request_id(self, builder: TestResultsBuilder, test_results: DownloadTestData):
+    def test_it_has_request_id(self, builder: Builder, test_results: DownloadTestData):
       assert builder.request_id == test_results['request_id']
 
-    def test_it_has_received_response(self, builder: TestResultsBuilder, test_results: DownloadTestData):
+    def test_it_has_received_response(self, builder: Builder, test_results: DownloadTestData):
       assert builder.received_response == test_results['received_response']
 
-    def test_it_has_skipped(self, builder: TestResultsBuilder, test_results: DownloadTestData):
+    def test_it_has_skipped(self, builder: Builder, test_results: DownloadTestData):
       assert builder.skipped == test_results['skipped']
 
-    def test_it_has_status(self, builder: TestResultsBuilder, test_results: DownloadTestData):
+    def test_it_has_status(self, builder: Builder, test_results: DownloadTestData):
       assert builder.status == test_results['status']
 
-    def test_it_has_strategy(self, builder: TestResultsBuilder, test_results: DownloadTestData):
+    def test_it_has_strategy(self, builder: Builder, test_results: DownloadTestData):
       assert builder.strategy == test_results['strategy']
 
   class TestWhenEmptyResponse():
@@ -60,16 +60,16 @@ class TestTestResultsBuilder():
       }
 
     @pytest.fixture(scope='class')
-    def builder(self, test_results) -> TestResultsBuilder:
-      _builder = TestResultsBuilder(**test_results)
-      builder = TestResultsBuilder()
+    def builder(self, test_results) -> Builder:
+      _builder = Builder(**test_results)
+      builder = Builder()
       builder.deserialize(_builder.serialize())
       return builder
 
-    def test_it_has_expected_response(self, builder: TestResultsBuilder, test_results: DownloadTestData):
+    def test_it_has_expected_response(self, builder: Builder, test_results: DownloadTestData):
       assert builder.expected_response == test_results['expected_response']
 
-    def test_it_has_received_response(self, builder: TestResultsBuilder, test_results: DownloadTestData):
+    def test_it_has_received_response(self, builder: Builder, test_results: DownloadTestData):
       assert builder.received_response == test_results['received_response']
 
   class TestWhenTraversibleResponse():
@@ -81,14 +81,14 @@ class TestTestResultsBuilder():
       }
 
     @pytest.fixture(scope='class')
-    def builder(self, test_results) -> TestResultsBuilder:
-      _builder = TestResultsBuilder(**test_results)
-      builder = TestResultsBuilder()
+    def builder(self, test_results) -> Builder:
+      _builder = Builder(**test_results)
+      builder = Builder()
       builder.deserialize(_builder.serialize())
       return builder
 
-    def test_it_has_expected_response(self, builder: TestResultsBuilder, test_results: DownloadTestData):
+    def test_it_has_expected_response(self, builder: Builder, test_results: DownloadTestData):
       assert builder.expected_response == test_results['expected_response']
 
-    def test_it_has_received_response(self, builder: TestResultsBuilder, test_results: DownloadTestData):
+    def test_it_has_received_response(self, builder: Builder, test_results: DownloadTestData):
       assert builder.received_response == test_results['received_response']
