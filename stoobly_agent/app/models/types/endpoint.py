3b698b7b079889d31ef3b9b6a14ac86acc304ab3
Tyler Bhadra <tylerbhadra@berkeley.edu>
2024-03-25 22:47:22 -0700

    Modify endpoints_resource + add header_names_resource, body_param_names_resource, query_param_names_resource, response_param_names_resource, and response_header_names_resource resource classes

--- stoobly_agent/app/models/types/endpoint.py
+++ stoobly_agent/app/models/types/endpoint.py
@@ -3,5 +3,23 @@ from typing import Literal, TypedDict
 OPENAPI_FORMAT = 'openapi'
 
 class EndpointCreateParams(TypedDict):
-  format: Literal[f"{OPENAPI_FORMAT}"]
+  host: str
+  method: str
+  path_segments: str
   path: str
+  port: str
+  project_id: str
+
+class HeaderNameCreateParams(TypedDict):
+  name: str
+  is_required: bool
+  is_deterministic: bool
+  project_id: str
+  endpoint_id: int
+	
+class ParamNameCreateParams(TypedDict):
+	name: str
+	project_id: str
+	endpoint_id: int
+	inferred_type: str
+	query: str
