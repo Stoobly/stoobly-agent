aa959ec67678b3f5910af0cb659f1cd93e743cf5
stoobmmelier <87163625+stoobmmelier@users.noreply.github.com>
2025-02-12 08:07:34 -0800

    Enhance scaffold validate to check system hosts file (#293)
    * Enhance scaffold validate to check system hosts file
    
    * Add extra unit test
    
    * Use hostname instead of url for hosts file
    
    * Change connection check to socket

---
+++ stoobly_agent/test/app/cli/scaffold/hosts_file_reader_test.py
@@ -0,0 +1,79 @@
+import pdb
+import pytest
+
+from stoobly_agent.app.cli.scaffold.hosts_file_reader import HostsFileReader
+
+
+class TestHostsFileReader():
+
+  @pytest.fixture
+  def hosts_file_reader(self):
+    yield HostsFileReader()
+
+  def test_get_hosts_file_path(self, hosts_file_reader):
+    hosts_file_path = hosts_file_reader._HostsFileReader__get_hosts_file_path()
+
+    # Test runners are all Linux distros for now
+    assert hosts_file_path == '/etc/hosts'
+
+  def test_get_hosts(self, hosts_file_reader):
+    hosts = hosts_file_reader.get_hosts()
+
+    assert hosts
+    assert len(hosts) > 1
+
+    localhost_found = False
+    for host in hosts:
+      if host.ip_address == '127.0.0.1' and 'localhost' in host.hostnames:
+        localhost_found = True
+        break
+    assert localhost_found
+
+  def test_find_host(self, hosts_file_reader):
+    url = 'localhost'
+
+    host = hosts_file_reader.find_host(url)
+
+    assert host
+    assert host.ip_address == '127.0.0.1'
+    assert 'localhost' in host.hostnames
+
+
+  class TestsSplitHostsLine():
+    def test_basic(self, hosts_file_reader):
+      line = '0.0.0.0 example.com'
+      split = hosts_file_reader._HostsFileReader__split_hosts_line(line)
+
+      assert split[0] == '0.0.0.0'
+      assert split[1] == 'example.com'
+
+    def test_tabs(self, hosts_file_reader):
+      line = '0.0.0.0\texample.com'
+      split = hosts_file_reader._HostsFileReader__split_hosts_line(line)
+
+      assert split[0] == '0.0.0.0'
+      assert split[1] == 'example.com'
+
+    def test_comment(self, hosts_file_reader):
+      line = '# 0.0.0.0 example.com'
+      split = hosts_file_reader._HostsFileReader__split_hosts_line(line)
+
+      assert len(split) == 0
+
+    def test_inline_comment(self, hosts_file_reader):
+      line = '0.0.0.0\texample.com # Comment'
+      split = hosts_file_reader._HostsFileReader__split_hosts_line(line)
+
+      assert split[0] == '0.0.0.0'
+      assert split[1] == 'example.com'
+
+    def test_multiple_hostnames(self, hosts_file_reader):
+      line = '0.0.0.0\texample.com example2.com example3.com # Comment'
+      split = hosts_file_reader._HostsFileReader__split_hosts_line(line)
+
+      assert split[0] == '0.0.0.0'
+      assert split[1] == 'example.com'
+      assert split[2] == 'example2.com'
+      assert split[3] == 'example3.com'
+
+
