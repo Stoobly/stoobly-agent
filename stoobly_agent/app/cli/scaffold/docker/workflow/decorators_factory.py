aa160aa6d7221e422d4d9440aca08e987aaa8308
Jvlythical <lvlichael8@gmail.com>
2025-02-25 19:37:17 -0800

    314 workflow run should separately build images (#315)
    * Add dns property to mock workflow
    
    * Default test and mock workflow mock policy to all
    
    * Split docker compose up into docker compose build
    
    * Refactor

--- stoobly_agent/app/cli/scaffold/docker/workflow/decorators_factory.py
+++ stoobly_agent/app/cli/scaffold/docker/workflow/decorators_factory.py
@@ -1,6 +1,7 @@
 from stoobly_agent.app.cli.scaffold.service_config import ServiceConfig
 
 from ...constants import WORKFLOW_MOCK_TYPE, WORKFLOW_RECORD_TYPE
+from .development_decorator import DevelopmentDecorator
 from .mock_decorator import MockDecorator
 from .reverse_proxy_decorator import ReverseProxyDecorator
 
@@ -10,6 +11,11 @@ def get_workflow_decorators(workflow: str, service_config: ServiceConfig):
   if workflow == WORKFLOW_RECORD_TYPE:
     if service_config.hostname:
       workflow_decorators.append(ReverseProxyDecorator)
+      workflow_decorators.append(DevelopmentDecorator)
+  elif workflow == WORKFLOW_MOCK_TYPE:
+     if service_config.hostname:
+      workflow_decorators.append(ReverseProxyDecorator if service_config.detached else MockDecorator)    
+      workflow_decorators.append(DevelopmentDecorator)
   else:
     if service_config.hostname:
       workflow_decorators.append(ReverseProxyDecorator if service_config.detached else MockDecorator) 
