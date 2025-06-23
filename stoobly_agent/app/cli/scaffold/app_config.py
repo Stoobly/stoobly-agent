f5c4c1c281ced7442ec78ca1b5e23396c379d514
jvlythical <lvlichael8@gmail.com>
2025-06-23 12:29:24 -0700

    Add --ui-port option to scaffold app create command

--- stoobly_agent/app/cli/scaffold/app_config.py
+++ stoobly_agent/app/cli/scaffold/app_config.py
@@ -1,5 +1,5 @@
 from .config import Config
-from .constants import APP_NAME_ENV, APP_NETWORK_ENV
+from .constants import APP_NAME_ENV, APP_NETWORK_ENV, APP_UI_PORT_ENV
 
 class AppConfig(Config):
 
@@ -27,11 +27,20 @@ class AppConfig(Config):
   def network(self, v):
     self.__network = v
 
+  @property
+  def ui_port(self):
+    return self.__ui_port
+
+  @ui_port.setter
+  def ui_port(self, v):
+    self.__ui_port = v
+
   def load(self, config = None):
     config = config or self.read()
 
     self.name = config.get(APP_NAME_ENV)
     self.network = config.get(APP_NETWORK_ENV)
+    self.ui_port = config.get(APP_UI_PORT_ENV)
     
   def write(self):
     config = {}
@@ -41,5 +50,8 @@ class AppConfig(Config):
 
     if self.network:
       config[APP_NETWORK_ENV] = self.network
+
+    if self.ui_port:
+      config[APP_UI_PORT_ENV] = self.ui_port
 
     super().write(config)
