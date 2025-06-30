57f86a39ddacf6f8a939ff6d7ce98a341aa8c300
jvlythical <lvlichael8@gmail.com>
2025-06-30 00:22:58 -0700

    Validate namespace

--- stoobly_agent/app/cli/ca_cert_cli.py
+++ stoobly_agent/app/cli/ca_cert_cli.py
@@ -15,8 +15,18 @@ def ca_cert(ctx):
     pass
 
 @ca_cert.command()
-@click.option('--ca-certs-dir-path', default=DataDir.instance().ca_certs_dir_path, help='Path to ca certs directory used to sign SSL certs.')
-@click.option('--certs-dir-path', default=DataDir.instance().certs_dir_path, help='Output directory.')
+@click.option(
+    '--ca-certs-dir-path', 
+    default=DataDir.instance().ca_certs_dir_path, 
+    help='Path to ca certs directory used to sign SSL certs.',
+    type=click.Path(exists=True, file_okay=False, dir_okay=True)
+)
+@click.option(
+    '--certs-dir-path',
+    default=DataDir.instance().certs_dir_path,
+    help='Output directory.',
+    type=click.Path(exists=True, file_okay=False, dir_okay=True)
+)
 @click.argument('hostname')
 def mkcert(**kwargs):
     ca_certs_dir_path = kwargs['ca_certs_dir_path']
@@ -30,7 +40,12 @@ def mkcert(**kwargs):
         sys.exit(1)
 
 @ca_cert.command()
-@click.option('--ca-certs-dir-path', default=DataDir.instance().ca_certs_dir_path, help='Path to ca certs directory.')
+@click.option(
+    '--ca-certs-dir-path',
+    default=DataDir.instance().ca_certs_dir_path,
+    help='Path to ca certs directory.',
+    type=click.Path(exists=True, file_okay=False, dir_okay=True)
+)
 def install(**kwargs):
     ca_certs_dir_path = kwargs['ca_certs_dir_path']
     installer = CertificateAuthority(ca_certs_dir_path)
---
+++ stoobly_agent/app/cli/validators/scaffold.py
@@ -0,0 +1,34 @@
+import re
+import sys
+
+def validate_app_name(ctx, param, app_name: str) -> str:
+  app_name_regex = re.compile(r'^[a-zA-Z0-9._-]+$')
+  if not re.search(app_name_regex, app_name):
+    print(f"Error: app name {app_name} is invalid.", file=sys.stderr)
+    sys.exit(1)
+  return app_name
+
+def validate_hostname(ctx, param, hostname: str) -> str:
+  if not hostname:
+    return
+  hostname_regex = re.compile(r'^[a-zA-Z0-9.-]+$')
+  if not re.search(hostname_regex, hostname):
+    print(f"Error: hostname {hostname} is invalid.", file=sys.stderr)
+    sys.exit(1)
+  return hostname
+
+def validate_namespace(ctx, param, namespace: str) -> str:
+  if not namespace:
+    return
+  namespace_regex = re.compile(r'^[a-z0-9_-]+$')
+  if not re.search(namespace_regex, namespace) or namespace[0] in ['-', '_']:
+    print(f"Error: namespace {namespace} is invalid.", file=sys.stderr)
+    sys.exit(1)
+  return namespace
+
+def validate_service_name(ctx, param, service_name: str) -> str:
+  service_name_regex = re.compile(r'^[a-zA-Z0-9._-]+$')
+  if not re.search(service_name_regex, service_name):
+    print(f"Error: service name {service_name} is invalid.", file=sys.stderr)
+    sys.exit(1)
+  return service_name
