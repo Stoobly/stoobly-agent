b0f5bbd5af0b1a6bcb5676556358c7ba002fa318
jvlythical <lvlichael8@gmail.com>
2025-08-11 22:38:24 -0700

    Created detached decorator

--- stoobly_agent/app/cli/scaffold/docker/workflow/decorators_factory.py
+++ stoobly_agent/app/cli/scaffold/docker/workflow/decorators_factory.py
@@ -1,6 +1,7 @@
 from stoobly_agent.app.cli.scaffold.service_config import ServiceConfig
 
 from ...constants import WORKFLOW_MOCK_TYPE, WORKFLOW_RECORD_TYPE, WORKFLOW_TEST_TYPE
+from .detached_decorator import DetachedDecorator
 from .dns_decorator import DnsDecorator
 from .mock_decorator import MockDecorator
 from .reverse_proxy_decorator import ReverseProxyDecorator
@@ -14,10 +15,10 @@ def get_workflow_decorators(workflow: str, service_config: ServiceConfig):
       workflow_decorators.append(DnsDecorator)
   elif workflow == WORKFLOW_MOCK_TYPE or workflow == WORKFLOW_TEST_TYPE:
      if service_config.hostname:
-      workflow_decorators.append(ReverseProxyDecorator if service_config.detached else MockDecorator)    
+      workflow_decorators.append(DetachedDecorator if service_config.detached else MockDecorator)
       workflow_decorators.append(DnsDecorator)
   else:
     if service_config.hostname:
-      workflow_decorators.append(ReverseProxyDecorator if service_config.detached else MockDecorator) 
+      workflow_decorators.append(DetachedDecorator if service_config.detached else MockDecorator) 
 
   return workflow_decorators
---
+++ stoobly_agent/app/cli/scaffold/docker/workflow/detached_decorator.py
@@ -0,0 +1,45 @@
+import os
+import pdb
+
+from ...constants import SERVICE_HOSTNAME, SERVICE_PORT, SERVICE_PROXY_MODE, STOOBLY_CERTS_DIR
+from .builder import WorkflowBuilder
+
+class DetachedDecorator():
+
+  def __init__(self, workflow_builder: WorkflowBuilder):
+    self.__workflow_builder = workflow_builder
+
+  @property
+  def workflow_builder(self):
+    return self.__workflow_builder
+
+  @property
+  def service_builder(self):
+    return self.workflow_builder.service_builder
+
+  def decorate(self):
+    config = self.service_builder.config
+
+    command = [
+      '--headless',
+      '--lifecycle-hooks-path', 'lifecycle_hooks.py',
+      '--proxy-mode', SERVICE_PROXY_MODE,
+      '--proxy-port', f"{SERVICE_PORT}",
+      '--public-directory-path', 'public',
+      '--response-fixtures-path', 'fixtures.yml',
+      '--ssl-insecure'
+    ]
+
+    if config.scheme == 'https':
+      command.append('--certs')
+      command.append(os.path.join(STOOBLY_CERTS_DIR, f"{SERVICE_HOSTNAME}-joined.pem"))
+
+    services = self.__workflow_builder.services
+    proxy_name = self.__workflow_builder.proxy
+    proxy_service = services.get(proxy_name) or {}
+
+    services[proxy_name] = { 
+      **proxy_service,
+      **{ 'command': command },
+    }
+
