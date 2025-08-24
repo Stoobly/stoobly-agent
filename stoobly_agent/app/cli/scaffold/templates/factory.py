b228498143d0260c7f539759e3db78ec39a9e7f1
jvlythical <lvlichael8@gmail.com>
2025-08-23 18:26:04 -0700

    Refactor template factory to conditionally include Docker files

--- stoobly_agent/app/cli/scaffold/templates/factory.py
+++ stoobly_agent/app/cli/scaffold/templates/factory.py
@@ -1,44 +1,53 @@
 import pdb
 
 from ..constants import WORKFLOW_MOCK_TYPE, WORKFLOW_RECORD_TYPE, WORKFLOW_TEST_TYPE
-from ..docker.workflow.builder import WorkflowBuilder
+from ..workflow_builder import WorkflowBuilder
+from ..docker.workflow.builder import DockerWorkflowBuilder
 from .constants import (
-  CUSTOM_CONFIGURE, CUSTOM_INIT, CUSTOM_PUBLIC_GITIGNORE, MAINTAINED_CONFIGURE, MOCK_WORKFLOW_CUSTOM_FILES, MOCK_WORKFLOW_MAINTAINED_FILES, RECORD_WORKFLOW_CUSTOM_FILES, RECORD_WORKFLOW_MAINTAINED_FILES, TEST_WORKFLOW_CUSTOM_FILES, TEST_WORKFLOW_MAINTAINED_FILES
+  CUSTOM_CONFIGURE, CUSTOM_INIT, CUSTOM_PUBLIC_GITIGNORE, MAINTAINED_CONFIGURE, 
+  MOCK_WORKFLOW_CUSTOM_FILES, MOCK_WORKFLOW_MAINTAINED_FILES, MOCK_WORKFLOW_CUSTOM_DOCKER_FILES,
+  RECORD_WORKFLOW_CUSTOM_FILES, RECORD_WORKFLOW_MAINTAINED_FILES, RECORD_WORKFLOW_CUSTOM_DOCKER_FILES,
+  TEST_WORKFLOW_CUSTOM_FILES, TEST_WORKFLOW_MAINTAINED_FILES, TEST_WORKFLOW_CUSTOM_DOCKER_FILES
 )
 
-def custom_files(workflow: str, workflow_builder: WorkflowBuilder):
+def custom_files(workflow: str, workflow_builder: WorkflowBuilder = None):
   files = []
   if workflow == WORKFLOW_MOCK_TYPE:
-    files = MOCK_WORKFLOW_CUSTOM_FILES
+    files = MOCK_WORKFLOW_CUSTOM_FILES.copy()
   elif workflow == WORKFLOW_RECORD_TYPE:
-    files = RECORD_WORKFLOW_CUSTOM_FILES
+    files = RECORD_WORKFLOW_CUSTOM_FILES.copy()
   elif workflow == WORKFLOW_TEST_TYPE:
-    files = TEST_WORKFLOW_CUSTOM_FILES
+    files = TEST_WORKFLOW_CUSTOM_FILES.copy()
   else:
-    if workflow_builder.configure in workflow_builder.services:
-      files.append(CUSTOM_CONFIGURE)
+    files.append(CUSTOM_CONFIGURE)
+    files.append(CUSTOM_INIT)
 
-    if workflow_builder.init in workflow_builder.services:
-      files.append(CUSTOM_INIT)
+  # Add Docker-specific files if workflow_builder is a DockerWorkflowBuilder
+  if isinstance(workflow_builder, DockerWorkflowBuilder):
+    if workflow == WORKFLOW_MOCK_TYPE:
+      files.extend(MOCK_WORKFLOW_CUSTOM_DOCKER_FILES)
+    elif workflow == WORKFLOW_RECORD_TYPE:
+      files.extend(RECORD_WORKFLOW_CUSTOM_DOCKER_FILES)
+    elif workflow == WORKFLOW_TEST_TYPE:
+      files.extend(TEST_WORKFLOW_CUSTOM_DOCKER_FILES)
 
   # Fixtures are only relevant if the workflow is mock/test and if the service has a hostname
-  if not workflow_builder.config.hostname:
+  if workflow_builder and not workflow_builder.config.hostname:
     if CUSTOM_PUBLIC_GITIGNORE in files:
       files.remove(CUSTOM_PUBLIC_GITIGNORE)
 
   return files
 
-def maintained_files(workflow: str, workflow_builder: WorkflowBuilder):
+def maintained_files(workflow: str, workflow_builder: WorkflowBuilder = None):
   files = []
 
   if workflow == WORKFLOW_MOCK_TYPE:
-    files = MOCK_WORKFLOW_MAINTAINED_FILES
+    files = MOCK_WORKFLOW_MAINTAINED_FILES.copy()
   elif workflow == WORKFLOW_RECORD_TYPE:
-    files = RECORD_WORKFLOW_MAINTAINED_FILES
+    files = RECORD_WORKFLOW_MAINTAINED_FILES.copy()
   elif workflow == WORKFLOW_TEST_TYPE:
-    files = TEST_WORKFLOW_MAINTAINED_FILES
+    files = TEST_WORKFLOW_MAINTAINED_FILES.copy()
   else:
-    if workflow_builder.configure in workflow_builder.services:
-      files.append(MAINTAINED_CONFIGURE)
+    files.append(MAINTAINED_CONFIGURE)
 
   return files
