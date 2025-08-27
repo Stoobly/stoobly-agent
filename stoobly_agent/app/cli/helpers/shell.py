3b6643a2eb8012dbf9598a892727e4570027e861
jvlythical <lvlichael8@gmail.com>
2025-08-26 20:16:01 -0700

    Update exec_stream to run commands in foreground for proper signal handling

--- stoobly_agent/app/cli/helpers/shell.py
+++ stoobly_agent/app/cli/helpers/shell.py
@@ -4,23 +4,6 @@ import sys
 DOTENV_PATH = 'STOOBLY_DOTENV_PATH'
 
 def exec_stream(command):
-  # Start the process
-  process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-  
-  # Stream output line by line
-  while True:
-      stdout = process.stdout.readline()
-      stderr = process.stderr.readline()
-
-      if stdout == '' and stderr == '' and process.poll() is not None:
-          break
-
-      if stdout:
-          print(stdout, end='')
-
-      if stderr:
-          print(stderr, end='', file=sys.stderr)
-  
-  # Get the remaining output (if any)
-  rc = process.poll()
-  return rc
+  # Run the command in the foreground so Ctrl+C properly exits
+  process = subprocess.run(command, shell=True)
+  return process.returncode
