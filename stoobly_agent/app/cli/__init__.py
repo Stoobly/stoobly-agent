739d5d4b23fa0a5b4f7d032bad7965b35c3279d6
stoobmmelier <87163625+stoobmmelier@users.noreply.github.com>
2025-09-17 17:16:59 -0700

    Fix build

--- stoobly_agent/app/cli/__init__.py
+++ stoobly_agent/app/cli/__init__.py
@@ -4,7 +4,6 @@ from .dev_tools_cli import dev_tools
 from .endpoint_cli import endpoint
 from .feature_cli import feature
 from .intercept_cli import intercept
-from .log_cli import log
 from .main_group import MainGroup
 from .project_cli import project
 from .report_cli import report
