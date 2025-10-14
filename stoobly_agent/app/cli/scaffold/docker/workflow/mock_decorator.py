5b0d33b265131baa5dbc4a0b44559adc687cfd2f
stoobmmelier <87163625+stoobmmelier@users.noreply.github.com>
2025-10-13 23:41:41 -0700

    Remove scaffold log level since it's the default already

--- stoobly_agent/app/cli/scaffold/docker/workflow/mock_decorator.py
+++ stoobly_agent/app/cli/scaffold/docker/workflow/mock_decorator.py
@@ -35,8 +35,6 @@ class MockDecorator(CommandDecorator):
       command.append(os.path.join(STOOBLY_CERTS_DIR, f"{SERVICE_HOSTNAME}-joined.pem"))
 
     command.append('--request-log-enable')
-    command.append('--request-log-level')
-    command.append('info')
 
     services = self.workflow_builder.services
     proxy_name = self.workflow_builder.proxy
