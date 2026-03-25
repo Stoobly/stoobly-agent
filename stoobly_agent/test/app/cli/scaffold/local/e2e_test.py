38c60c6c3ba24b324adb98fc5edfc643544acc00
J3utter <j3utter@gmail.com>
2026-03-24 22:51:59 -0700

    Fix failing test

--- stoobly_agent/test/app/cli/scaffold/local/e2e_test.py
+++ stoobly_agent/test/app/cli/scaffold/local/e2e_test.py
@@ -352,10 +352,10 @@ class TestLocalScaffoldE2e():
       assert external_service_name in services, f"External service {external_service_name} should exist"
       assert local_service_name in services, f"Local service {local_service_name} should exist"
 
-    def test_workflow_up(self, proxy_url: str):
+    def test_workflow_up(self, hostname: str, proxy_url: str):
       """Test workflow up command for local execution"""
       # Use http send request with proxy, do this in python, make the following not check ssl cert
-      res = requests.get('https://docs.stoobly.com', proxies={'http': proxy_url, 'https': proxy_url}, verify=False)
+      res = requests.get('http://' + hostname, proxies={'http': proxy_url, 'https': proxy_url}, verify=False)
       assert res.status_code == 499, "HTTP request with HTTP_PROXY and HTTPS_PROXY set to the local service should succeed"
 
     def test_intercept_mode_mock(self, settings: Settings):
