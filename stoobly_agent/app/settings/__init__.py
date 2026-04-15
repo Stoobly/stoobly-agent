c88a1a58860f8512b2d3a78d35581d635c4361c6
stoobmmelier <87163625+stoobmmelier@users.noreply.github.com>
2026-04-15 16:38:22 -0700

    Test using PollingObserver for flaky tests

--- stoobly_agent/app/settings/__init__.py
+++ stoobly_agent/app/settings/__init__.py
@@ -122,8 +122,12 @@ class Settings:
         )
         event_handler.on_modified = self.__reload_settings
 
-        from watchdog.observers import Observer
-        observer = Observer()
+        # from watchdog.observers import Observer
+        # observer = Observer()
+
+        from watchdog.observers.polling import PollingObserver
+        observer = PollingObserver(timeout=0.1)
+
         watch_dir = os.path.dirname(self.__settings_file_path)
 
         observer.schedule(event_handler, watch_dir)
